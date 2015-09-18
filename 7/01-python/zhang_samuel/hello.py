7783d4a36b957cc447cd1fa4da0f51ab5b197360
Samuel Zhang <samuelzhang3@gmail.com>
2015-09-18 17:27:51 -0400

    first commit

--- 7/01-python/zhang_samuel/hello.py
+++ 7/01-python/zhang_samuel/hello.py
@@ -1,1 +1,26 @@
-print "hello"
+import random
+
+#print "hello"
+
+suit = ["Diamonds", "Clubs", "Hearts", "Spades"]
+number = []
+
+for i in xrange(0, 13):
+    number.append(i)
+
+rSuit = random.randint(0, 3)
+rNum = random.randint(1, 13)
+s = ""
+
+if(rNum > 10):
+    if(rNum == 11):
+      s = "Jack"
+    elif(rNum == 12):
+        s = "Queen"
+    else:
+        s = "King"
+    print s,
+else:
+    print rNum,
+
+print "of " + suit[rSuit]
