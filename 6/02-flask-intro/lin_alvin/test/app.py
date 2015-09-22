bdb277a9bef027e77d78ac665581afd932ca9303
Alvin Lin <alvin.lin@stuypulse.com>
2015-09-22 12:33:48 -0400

    templates

--- 6/02-flask-intro/lin_alvin/test/app.py
+++ 6/02-flask-intro/lin_alvin/test/app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
+def default():
+  return '<a href="/test">test</a>'
+
+@app.route('/test')
 def test():
-  return 'Hello World'
+  return render_template('index.html', variable='hi')
 
 if __name__ == '__main__':
   app.run()
