a50c6713f7cef0735a6d21d5e57cb55c2a518181
li-cindy <li.cindy98@gmail.com>
2015-09-24 12:32:50 -0400

    1/3 templates complete

---
+++ 7/02-flask-intro/li_cindy/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/aboutpuppies")
+def puppies():
+    return render_template("aboutpuppies.html")
+
+def anna():
+    return render_template("aboutanna.html")
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 8000)
