323266cb2d29b47774542e43207d5efe405d3089
az0977776 <az0977776@yahoo.com>
2015-09-24 19:37:17 -0400

    templates

---
+++ 6/02-flask-intro/wang_aaron/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/page1")
+def page1():
+    return render_template("page1.html")
+
+@app.route("/page2")
+def page2():
+    return render_template("page2.html")
+
+if __name__=="__main__":
+    app.debug = True
+    app.run(host='0.0.0.0',port=8000)
