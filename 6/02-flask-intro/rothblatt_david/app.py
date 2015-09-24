c6a046967bfa07796445763a65d9422adbc0ca7e
David Rothblatt <daroth13@gmail.com>
2015-09-24 18:52:36 -0400

    slight tweaks to files

--- 6/02-flask-intro/rothblatt_david/app.py
+++ 6/02-flask-intro/rothblatt_david/app.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("")
 @app.route("/home")
 def home():
     return render_template("home.html")
@@ -12,8 +11,8 @@ def home():
 def funny():
     return render_template("funny.html")
 
-
-app.debug = True
-app.run(host = '0.0.0.0', port = 8000)
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 8000)
 
 
