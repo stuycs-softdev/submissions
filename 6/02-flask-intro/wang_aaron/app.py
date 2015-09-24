c5d01b4e82d5c9cf2339ae14b1dc6143fd712b86
az0977776 <az0977776@yahoo.com>
2015-09-24 19:58:23 -0400

    added pages

--- 6/02-flask-intro/wang_aaron/app.py
+++ 6/02-flask-intro/wang_aaron/app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("homepage.html")
+
 @app.route("/page1")
 def page1():
     return render_template("page1.html")
