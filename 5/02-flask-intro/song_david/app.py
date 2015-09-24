fb2cf0a17959bc18decd194773a42c26183056fd
David Song <songdavid98@gmail.com>
2015-09-24 18:02:17 -0400

    flask

--- 5/02-flask-intro/song_david/app.py
+++ 5/02-flask-intro/song_david/app.py
@@ -6,11 +6,15 @@ app = Flask(__name__)
 def about():
     return render_template("about.html")
 
+@app.route("/hello")
+def hello():
+    return render_template("hello.html")
+
 @app.route("/home")
 @app.route("/")
 def home():
-    return "/home"
+    return render_template("home.html")
 
-    if __name__ == "__main__":
-        app.debug = True
-        app.run(host = '0.0.0.0', port = 8000)
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 8000)
