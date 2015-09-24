dbd0346befd5bf92ef14cfd45313b97e59dc59dd
David Rothblatt <daroth13@gmail.com>
2015-09-24 18:16:47 -0400

    basics of app.py

--- 6/02-flask-intro/rothblatt_david/app.py
+++ 6/02-flask-intro/rothblatt_david/app.py
@@ -1,1 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/funny")
+def funny():
+    return render_template("funny.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 8000)
+
+
