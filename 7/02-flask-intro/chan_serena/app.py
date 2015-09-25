c2e41bd89385ee0da5cc60ff40017b7de4fa25a5
Serena Chan <serenagchan@gmail.com>
2015-09-25 00:47:50 -0400

    HW2

--- 7/02-flask-intro/chan_serena/app.py
+++ 7/02-flask-intro/chan_serena/app.py
@@ -6,12 +6,17 @@ app = Flask(__name__)
 @app.route("/home")
 
 def home():
-    text = "<h1><font color="blue">Home</font></h1>"
+    text = """<h1><font color="blue">Home</font></h1>
+    <font face="Brush Script MT" size="+3" color="#FF0000"> Welcome</font>"""
     return text
 
 @app.route("/page2")
 def page2():
     return render_template("page2.html")
+
+@app.route("/page3")
+def page3():
+    return render_template("page3.html")
 
 if __name__== "__main__":
     app.debug = True
