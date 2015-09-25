5134a7662d5bb8ec638e51750ef2ab17a5a6e655
Wayez <c.wayez3793@gmail.com>
2015-09-25 01:48:28 +0000

    Finished app

--- 5/02-flask-intro/chowdhury_wayez/app.py
+++ 5/02-flask-intro/chowdhury_wayez/app.py
@@ -2,15 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app_route("/")
+@app.route("/")
 def page1():
     return render_template("home.html")
 
-@app_route("/about")
+@app.route("/about")
 def about():
     return render_template("about.html")
 
 if __name__ == "__main__":
     app.debug = True
-    app.run(host = '0.0.0.0', port = 8000)
+    app.run(host = '0.0.0.0')
 
