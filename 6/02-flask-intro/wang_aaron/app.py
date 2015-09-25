5709f60f25ab5c82b90c2b44c46fede1fb74994c
Aaron Wang <az0977776@yahoo.com>
2015-09-25 12:30:34 -0400

    update

--- 6/02-flask-intro/wang_aaron/app.py
+++ 6/02-flask-intro/wang_aaron/app.py
@@ -8,11 +8,16 @@ def home():
 
 @app.route("/page1")
 def page1():
-    return render_template("page1.html")
+    import random
+    r = random.randrange(0,100)
+    return render_template("page1.html",number = r)
 
 @app.route("/page2")
-def page2():
-    return render_template("page2.html")
+@app.route("/<firstname>/<lastname>")
+def page2(lastname = "", firstname=""):
+    d = {'first':firstname,'last':lastname}
+    return render_template("page2.html",d = d)
+
 
 if __name__=="__main__":
     app.debug = True
