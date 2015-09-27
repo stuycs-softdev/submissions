7a19d82fd5f09ec175b44ce31bb001eaca6a3344
Sally <sallybao29@gmail.com>
2015-09-27 13:56:50 -0400

    added more routes

--- 6/02-flask-intro/bao_sally/app.py
+++ 6/02-flask-intro/bao_sally/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+ans = {"ans1": "hummingbird moth", "ans2": "", "ans3": ""}
 
 @app.route("/")
 @app.route("/home")
@@ -12,6 +13,29 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/guess")
+@app.route("/guess/")
+@app.route("/guess/<answer>")
+def guess(answer = ""):
+    result = ""
+    if answer == ans["ans1"]:
+        result = "Correct!"
+    elif answer != "":
+        result = "Incorrect. Please try again"
+    d = {"guess": answer, 
+             "result": result}
+    return render_template("guess.html", d = d)
+
+@app.route("/guess2")
+@app.route("/guess2/")
+@app.route("/guess2/<answer>")
+def guess2(answer = ""):
+    return render_template("guess2.html")
+
+
+
 
 if __name__ == "__main__":
     app.debug = True
