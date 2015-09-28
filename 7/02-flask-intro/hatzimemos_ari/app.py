46ad182e21017589d68c88ac64bcfee733e4bae5
Ari Hatzimemos <hatzimemosari3@gmail.com>
2015-09-28 01:05:43 -0400

    finished

--- 7/02-flask-intro/hatzimemos_ari/app.py
+++ 7/02-flask-intro/hatzimemos_ari/app.py
@@ -15,6 +15,21 @@ def home():
 def profile(lastname="",firstname="",title=""):
     return render_template("profile.html",firstname=firstname,lastname=lastname,title=title)
 
+@app.route("/nameTable")
+@app.route("/nameTable/")
+@app.route("/nameTable/<lastname>/<firstname>/<title>")
+def nameTable(lastname="",firstname="",title=""):
+    dictionary = {'last' : lastname,
+                  'first' :firstname,
+                  'title' :title}
+    return render_template("nameTable.html",d=dictionary)
+
+@app.route("/random")
+def random():
+    import random
+    num = random.randrange(0,100)
+    return render_template("random.html", n = num)
+
 @app.route("/table")
 def table():
     return render_template("table.html")
