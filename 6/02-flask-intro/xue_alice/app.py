f9a62207c1dad5aa71e383d5aac60fd7c5628198
Alice Xue <alicex123@gmail.com>
2015-09-29 12:34:25 -0400

    added classwork

--- 6/02-flask-intro/xue_alice/app.py
+++ 6/02-flask-intro/xue_alice/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,6 +9,10 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.debug = True
