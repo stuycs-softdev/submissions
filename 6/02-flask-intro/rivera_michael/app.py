53435306c3466901e3e5fb11901479927fbafb9c
MGRiv <mgrivera501@gmail.com>
2015-09-28 22:33:47 -0400

    login form

--- 6/02-flask-intro/rivera_michael/app.py
+++ 6/02-flask-intro/rivera_michael/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import random
 
 app = Flask(__name__)
@@ -26,6 +26,16 @@ def daily():
     r = random.randrange(0,9)
     l = ["Favorable","Not sure","Favorable","Favorable","Unfavorable","Not sure","Unfavorable","Not sure","Favorable","Unfavorable"]
     return render_template("daily.html",mes = l[r])
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        uname = request.form['uname']
+        pword = request.form['pword']
+        buton = request.form['button']
+        return render_template("twlight.html")
 
 
 if __name__ == "__main__":
