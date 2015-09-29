64fffbc2d29054b6e855ef32f8c90fcb513e7daa
Wayez <c.wayez3793@gmail.com>
2015-09-29 02:31:40 -0400

    Fixed bug and added a few new features

--- 5/02-flask-intro/chowdhury_wayez/app.py
+++ 5/02-flask-intro/chowdhury_wayez/app.py
@@ -1,13 +1,6 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
-likes = ["Pizza", "Tigers", "Babies", "Basketball", "Spongebob", "Movies", 
-         "Pokemon", "Turtles", "Ice Cream", "Computer Science", "Batman",
-         "Whales", "Baseball", "Anime", "Football", "Riddles", "Apples",
-         "Sour Candy"]
-
-dislikes = {1 : "fish", 2 : "Drafting", 3 : "Donald Trump"}
   
 @app.route("/")
 def page1():
@@ -19,8 +12,40 @@ def about():
 
 @app.route("/interests")
 def likes():
-    return render_template("interests.html", name = "Wayez", l = likes, 
-                           d = dislikes)
+    import random;
+    rand = random.randint(0,22)
+    interests = ["Pizza", "Tigers", "Basketball", "Spongebob", "Babies",
+                 "Movies", "Pokemon", "Turtles", "Ice Cream", "T-Rexs",
+                 "Computer Science", "Batman", "Whales", "Baseball", "Anime",
+                 "Riddles", "Apples", "Sour Candy", "Mr. K's Shirts", 
+                 "Birds", "Astronomy", "Murica", "Snow"]
+    name = interests[rand]
+    if rand == 8:
+        name = "IceCream"
+    elif rand == 10:
+        name = "CS"
+    elif rand == 17:
+        name = "Sour"
+    elif rand == 18:
+        name = "K"
+    u = "/static/" + name + ".jpg"
+    dislikes = {1 : "Fish", 2 : "Sharp Objects", 3 : "Drafting",
+                4 : "Telletubbies", 5 : "Donald Trump", 6 : "Cakes", 
+                7 : "Noises"}
+    ##anime red, astronomy white, basketball white, batman white, cs red,
+    ##movies white, pokemon white, riddle white, tiger trex white, sponge red
+    c = "black"
+    white = [20, 2, 11, 5, 6, 15, 9, 1]
+    red = [14, 10, 3]
+    for x in white:
+        if rand == x:
+            c = "white"
+    for y in red:
+        if rand == y:
+            c = "red"
+    return render_template("interests.html", name = "Wayez", 
+                           interests = interests, dislikes = dislikes, u = u,
+                           r = rand, c = c)
 
 if __name__ == "__main__":
     app.debug = True
