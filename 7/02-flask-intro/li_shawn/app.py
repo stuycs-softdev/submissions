a3b0ca92e32f3adb99eb45f0d1e839ab39bcd7fe
Shawn Li <shawnli050298@gmail.com>
2015-09-28 22:08:24 -0400

    Update app.py

--- 7/02-flask-intro/li_shawn/app.py
+++ 7/02-flask-intro/li_shawn/app.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import utils
+
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("about.html")
+
+@app.route("/login" methods = ["GET", "POST"])
+def login():
+    if request.method=="GET":
+        return render_template("login.html")
+    else:
+        uname = request.form['username']
+        pword = request.form['password']
+        button = request.form['button']
+        if button=="cancel":
+            return render_template("login.html")
+        if utils.authenticate(uname,pword):
+            return '<center><h1>Logged in</h1><br><img src = "http://i.imgur.com/LccckWB.gif"></center>'
+        else:
+            error = "Bad username or password"
+            return render_template("login.html",err=error)
+
+    
+    
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8000)
