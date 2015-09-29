fa00a55731a10767b84374b22a3bd1ff6155f658
Ho Yin Ho <hoyinho999@gmail.com>
2015-09-29 07:39:02 -0400

    fixed a few things

--- 5/02-flask-intro/ho_hoyin/app.py
+++ 5/02-flask-intro/ho_hoyin/app.py
@@ -2,15 +2,21 @@ from flask import Flask, render_template
 import random
 app = Flask(__name__)
 
+@app.route("/")
+@app.route("/<view>")
 @app.route("/<view>/<time>")
-def home(view = "YOU CAN'T SEE ME", time = "MY TIME IS NOW"):
+def home(view = "", time = ""):
+    if view == "none":
+        view = "YOU CAN'T SEE ME"
+    if time == "now":
+        time = "MY TIME IS NOW"
     stuff = {'view' : view , 'time' : time}
     return render_template("home.html", stuff = stuff)
 
 @app.route("/about")
 def about():
     num = random.randrange(0,100)
-    return render_template("about", num = num)
+    return render_template("about.html", num = num)
 
 
 if __name__ == "__main__":
