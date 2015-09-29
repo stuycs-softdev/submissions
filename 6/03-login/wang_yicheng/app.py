5e65aa10b92e2ad0be115aa2e0a151627aad4dac
Yicheng Wang <alex.wyc2098@protonmail.ch>
2015-09-29 19:39:39 -0400

    updated

--- 6/03-login/wang_yicheng/app.py
+++ 6/03-login/wang_yicheng/app.py
@@ -1,5 +1,42 @@
-from flask import Flask, render_template, session, redirect
+from flask import Flask, render_template, session, redirect, request
+from time import strftime
 
 app = Flask(__name__)
 
-@app.route('x')
+loginCreds = {'user' : 'password', 'user2' : '12345'}
+
+@app.route('/')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/login')
+def loginStatic():
+    return render_template("login.html")
+
+@app.route('/login', methods = ['POST'])
+def login():
+    name = request.form['name']
+    pwd = request.form['pwd']
+    if (name,pwd) in session:
+        session[name,pwd]
+        return redirect("/home/%s" % name)
+    if name in loginCreds:
+        if loginCreds[name] == pwd:
+            return redirect("/home/%s" % name)
+    return render_template("login.html", error = 1)
+
+@app.route('/home')
+def homeRedir():
+    return redirect("login")
+
+@app.route('/home/<username>')
+def home(username = ""):
+    if username not in loginCreds:
+        return redirect('/home')
+    return "<h1>HELLO</h1>"
+
+if __name__ == '__main__':
+    app.debug = True
+    app.secret_key = 'thisIsActuallyMyPassword'
+    app.run(host = '0.0.0.0', port = 8000)
