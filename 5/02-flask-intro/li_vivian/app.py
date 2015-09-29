6b9ed4ee83b0352f7b987391ab803feab781d3b7
VivianLi1 <vivian2938@gmail.com>
2015-09-29 11:46:21 -0400

    login

--- 5/02-flask-intro/li_vivian/app.py
+++ 5/02-flask-intro/li_vivian/app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/login")
+@app.route("/login/")
+def login():
+    return render_template("login.html")
+
 @app.route("/about")
 def about():
     return render_template("about.html") 
