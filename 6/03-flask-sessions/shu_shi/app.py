9219037c1eb0fe83d575f86383ebfe4aac8e1c48
Shi Shu <shishu_1998@yahoo.com>
2015-09-30 12:30:29 -0400

    classwork

--- 6/03-flask-sessions/shu_shi/app.py
+++ 6/03-flask-sessions/shu_shi/app.py
@@ -1,33 +1,38 @@
 from flask import Flask,render_template,request,sessions
 from flask import redirect,url_for
+import utils
 
 app = Flask(__name__)
 @app.route("/")
 
 @app.route("/homepage")
 def home():
-    button = request.form['button']
-    if button=="login here":
-        return render_template('login.html')
+        return render_template('homepage.html')
 
     
 
 @app.route("/login", methods = ["GET","POST"])
 def login():
     if request.method == "GET":
-        return render_template("homepage.html")
+        return render_template('login.html')
     else:
         username = request.form['username']
         password = request.form['password']
-        if utils.autheticate(username,password):
-            return render_template('secret.html')
+        button = request.form['button']
+        if button == "return home":
+            return redirect(url_for('homepage'))
+        if utils.authenticate(username,password):
+            return redirect(url_for('secret'))
         else:
-            return render_template('login.html')
+            return redirect(url_for('login'))
 
-@app.route("/secret")
+@app.route("/secret", methods = ["GET","POST"])
 def secret():
-    import random
-    rand = random.randrange(0,100)
+    if request.method == "GET":
+        return redirect(url_for('login'))
+    else:
+        import random
+        rand = random.randrange(0,100)
     return render_template("secret.html", rand = rand)
 
 if __name__ == "__main__":
