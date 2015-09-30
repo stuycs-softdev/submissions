c3d5b47a23863aaa1686179cadda9a00ce23cf6d
angelachan64 <angelachan64@gmail.com>
2015-09-29 23:13:53 -0400

    updated

--- 7/02-flask-intro/chan_angela/app.py
+++ 7/02-flask-intro/chan_angela/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 import util
 
 app = Flask(__name__)
@@ -10,7 +10,9 @@ def home():
     <button><a href="/about">To the About Page!</a></button>
     <button><a href="/pictures">Free Pictures!</a></button>
     <br> <br>
-    <button><a href="/login">Visit Your Profile</a></button>
+    <button><a href="/login">Login to Your Adoption Profile</a></button>
+    <br> <br>
+    <button><a href="/slogin">Login to Your Secret Profile</a></button>
     """
     return page
 
@@ -57,7 +59,51 @@ def login():
         else:
             error = "Your password did not contain 'cat' or 'dog'."
             return render_template("login.html",error=error)
+
+@app.route("/slogin", methods=["GET","POST"])
+def slogin():
+    if request.method=="GET":
+        return render_template("slogin.html")
+    else:
+        username = request.form["username"]
+        password = request.form["password"]
+        log = request.form["log"]
+        if log=="Cancel":
+            return render_template("slogin.html")
+        if util.logins(username,password):
+            return redirect(url_for("secret"))
+        else:
+            error = "Wrong username and password combination"
+            return render_template("slogin.html", error=error)
+
+@app.route("/secret")
+def secret():
+    session["buckle"] = 0
+    buckled=session["buckle"]
+    return render_template("secret.html", number=buckled)
+
+@app.route("/reset")
+def reset():
+    return redirect(url_for("secret"))
+
+@app.route("/okay")
+def okay():
+    buckled = 1
+    session["buckle"] = buckled
+    return render_template("secret.html", number=buckled)
+
+@app.route("/nope")
+def nope():
+    buckled = session["buckle"]
+    buckled = buckled - 1
+    session["buckle"] = buckled
+    return render_template("secret.html", number=buckled)
+
+@app.route("/logout")
+def logout():
+    return redirect(url_for("slogin"))
     
 if __name__ == "__main__":
     app.debug = True
+    app.secret_key = "BUCKLE YA PANTS"
     app.run(host='0.0.0.0',port=8000)
