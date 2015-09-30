0a489d011e5721548936e9126e3b044c2c0c1d70
MGRiv <mgrivera501@gmail.com>
2015-09-29 22:18:31 -0400

    just starting

--- 6/03-flask-sessions/rivera_michael/app.py
+++ 6/03-flask-sessions/rivera_michael/app.py
@@ -1,1 +1,18 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+@app.route("/about")
+def home():
+    return """<center>
+<h1>HELLO</h1>
+<br>
+<a href="http://localhost:8000/login">LOGIN</a>
+</center>"""
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+    if 'n' not in session:
+        session
