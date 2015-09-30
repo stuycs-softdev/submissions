9476ddecc6101f2c6cd3a5ae7d1dbefa28548dca
Jeanne Locker <jeannelocker@earthlink.net>
2015-09-30 12:32:54 -0400

    worked on things

--- 6/03-flask-sessions/locker_jeanne/app.py
+++ 6/03-flask-sessions/locker_jeanne/app.py
@@ -1,18 +1,34 @@
-from flask import Flask, render_tempalte, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, request
 
 app = Flask(__name__)
 
 @app.route("/about")
 def about():
-    
+    if 'n' not in session:
+        session['n']="logged out"
+    n = session['n']
+    n = "logged in"
+    session['n'] = n
+    return render_template("about.html",n=n)
 
 @app.route("/secret")
 def secret():
-    
+    return render_template("secret.html")
 
 @app.route("/login")
 def login():
-    
+    return render_template("login.html")
 
-@app.route("logout")
+@app.route("/gin")
+def gin():
+    if 'n' not in session:
+        session['n']="logged out"
+    n = session['n']
+    n = "logged in"
+    session['n'] = n
+    return render_template("secret.html",n=n)
+
+@app.route("/logout")
 def logout():
+    session['n']="logged out"
+    return redirect(url_for('about'))
