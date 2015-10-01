9c28d64ff9135767798027c27ae5ed8c5b0da808
Chris Liang <chrisliang98@gmail.com>
2015-10-01 11:50:57 -0400

    css

--- 5/03-flask-sessions/liang_christopher/app.py
+++ 5/03-flask-sessions/liang_christopher/app.py
@@ -50,7 +50,7 @@ def about():
 if __name__ == "__main__":
     app.debug = True
     app.secret_key="swag"
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=5000)
 
 #use dictionaries for templates 
 #for ex
