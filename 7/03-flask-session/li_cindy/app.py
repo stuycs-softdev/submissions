2b6c309ec69346f936afcf8941f1425a0ac95e77
li-cindy <li.cindy98@gmail.com>
2015-09-30 21:44:44 -0400

    logout still missing

---
+++ 7/03-flask-session/li_cindy/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+import utils
+
+app = Flask(__name__)
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+    if request.method=="GET":
+        return render_template("login.html")
+    else:
+        uname = request.form['username']
+        pword = request.form['password']
+        button = request.form['button']
+        if button=="cancel":
+            return render_template("login.html")
+        if utils.authenticate(uname,pword):
+            if 'logged' not in session: 
+                session['logged'] = True
+            return redirect(url_for("about"))
+        else:
+            error = "Invalid username or password"
+            return render_template("login.html",err=error)
+
+@app.route("/")
+def index():
+    print request.args
+    print request.args.get("size")
+    return render_template("home.html",args=request.args)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+if __name__ == "__main__":
+   app.debug = True
+   app.secret_key = "Don't put on git"
+   app.run(host="0.0.0.0", port=8600)
