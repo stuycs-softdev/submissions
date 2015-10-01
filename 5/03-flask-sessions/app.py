340b05312a85147c1afb4e07985d86f9255234bf
David Song <songdavid98@gmail.com>
2015-10-01 11:45:27 -0400

    updated app

--- 5/03-flask-sessions/app.py
+++ 5/03-flask-sessions/app.py
@@ -6,3 +6,15 @@ app = Flask(__name__)
 @app.route("/home")
 def home():
     return render_template("home.html")
+
+@app.route("/login", methods = ["GET","POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        return "HI"
+
+if __name__ == "__main__":
+    app.debug = True
+    app.secret_key = "ping"
+    app.run(port=8000)
