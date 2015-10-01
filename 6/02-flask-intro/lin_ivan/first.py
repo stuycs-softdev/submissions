e8ddde82a21e2543e60e1d3add0610e38ff90eb8
ivan <ivlin112@gmail.com>
2015-09-30 23:15:02 -0400

    Added stuff

--- 6/02-flask-intro/lin_ivan/first.py
+++ 6/02-flask-intro/lin_ivan/first.py
@@ -1,6 +1,33 @@
 from flask import Flask, render_template, session, request, redirect
 app = Flask(__name__)
 
+events = []		
+events.append({		
+    "name":"MOI'M Inception",		
+    "img":"http://linkis.com/url-image/https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s750x750/sh0.08/e35/11312158_1548465908742158_119971858_n.jpg",		
+    "address":"566 LaGuardia Place, New York, NY",		
+})		
+events.append({		
+    "name":"Mid-Autumn Moon Festival",		
+    "img":"https://scontent.xx.fbcdn.net/hphotos-xat1/v/t1.0-9/s720x720/11144915_836990889711969_7111602274309884526_n.jpg?oh=cf9539e2e7f3c16e067149e45678d65c&oe=5667B41C",		
+    "address":"79 Division Street, New York, NY 10002"		
+})		
+events.append({		
+    "name":"Waku Waku",		
+    "img":"http://www.japanculture-nyc.com/wp-content/uploads/2015/08/image3.jpg",		
+    "address":"72 Noble St Brooklyn , NY 11222"		
+})		
+events.append({		
+    "name":"New York City Comic Con 2015",		
+    "img":"http://www.newyorkcomiccon.com/RNA/RNA_NewYorkComicCon_V2/2015/images/_framework/nycc-logo-large.png?v=635670626456238965",		
+    "address":"Javist Center, 655 W 34th St, New York, NY 10001"		
+})		
+events.append({		
+    "name":"TEDxYouth: Made in the Future",		
+    "img":"https://yt3.ggpht.com/-yJAJEG_PrHk/AAAAAAAAAAI/AAAAAAAAAAA/nHtSZ9elZkM/s900-c-k-no/photo.jpg",		
+    "address":"Brooklyn Museum, 200 Eastern Pkwy, Brooklyn, NY 11238"		
+})
+
 @app.route("/")
 @app.route("/about")
 def about():
@@ -9,7 +36,7 @@ def about():
 @app.route("/secret")
 def secret():
   if session['user'] == "username":
-    return render_template("secret.html", events = [])
+    return render_template("secret.html", events = events)
   else:
     return redirect("/login")
 
@@ -22,11 +49,11 @@ def login():
             session['user'] = "username"
             return redirect("/secret")
         else:
-            return redirect("/about")
+            return redirect("/login")
 
 @app.route("/logout")
 def logout():
-    session['user'].pop()
+    session['user'] = ""
     return redirect("/login")
 
 
