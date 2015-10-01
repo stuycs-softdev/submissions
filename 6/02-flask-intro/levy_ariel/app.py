f403d3e28536fff29043eb9db55cfb675c042690
Ariel Levy <ariel.s.levy1@gmail.com>
2015-10-01 01:15:37 -0400

    hw 03

--- 6/02-flask-intro/levy_ariel/app.py
+++ 6/02-flask-intro/levy_ariel/app.py
@@ -2,22 +2,56 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+import datetime
+now = datetime.datetime.now()
+
 @app.route("/")
 @app.route("/home")
-def home():
-    return render_template("home.html")
+@app.route("/home/<n>")
+def home(n="Ariel"):
+    g = "Good evening"
+    if now.hour >= 5 and now.hour < 12:
+        g = "Good morning"
+    elif now.hour >= 12 and now.hour < 17:
+        g = "Good afternoon"
+    d = {'name':n, 'greeting':g}
+    return render_template("home.html",dict=d)
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/calendar")
+def calendar():
+    events = open("templates/calendar.txt").readlines()
+    cur = 0
+    while now.year < int(events[cur][6:10]):
+        cur += 1
+    while now.month < int(events[cur][3:5]):
+        cur += 1
+    while now.day < int(events[cur][0:2]):
+        cur += 1
+    t = "-".join(["%02d" % now.day, "%02d" % now.month, "%04d" % now.year])
+    e = ''.join(events[cur:])
+    d = {'today':t, 'evlist':e}
+    return render_template("calendar.html",dict=d)
 
-@app.route("/projects")
-def projects():
-    return render_template("projects.html")
+@app.route("/tasks")
+def tasks():
+    tasks = open("templates/tasks.txt").readlines()
+    td=""
+    dn=""
+    for i in tasks:
+        if i[0] == '#':
+            dn += i[1:]
+        else:
+            td += i
+    d = {'todo':td, 'done':dn}
+    return render_template("tasks.html",dict=d)
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
+@app.route("/countdown")
+def countdown():
+    date = datetime.datetime(2016, 1, 29, 3, 35, 0, 0)
+    dif = date - now
+    less = str(dif).split(" ")[2]
+    d = {'days':dif.days, 'hours':less[0:2], 'mins':less[3:5], 'secs':less[6:8]}
+    return render_template("countdown.html", dict=d)
 
 if __name__ == "__main__":
     app.debug = True
--- 6/03-flask-sessions/levy_ariel/app.py
+++ 6/03-flask-sessions/levy_ariel/app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask import redirect, url_for
+import utils
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    print "hello"
+    return render_template("index.html")
 
 @app.route("/about")
 def about():
@@ -14,14 +16,24 @@ def about():
 def secret():
     return render_template("secret.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET","POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        uname = request.form['username']
+        pword = request.form['password']
+        if utils.authenticate(uname, pword):
+            return render_template("secret.html")
+        else:
+            error = "Invalid username or password. Try again?"
+            return render_template("login.html",err=error)
 
 @app.route("/logout") # redirects to login page
 def logout():
-    return render_template("logout.html")
+    return redirect(url_for("index"))
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     app.debug = True
+    app.secret_key = "15651"
     app.run(host='0.0.0.0',port=8000)
