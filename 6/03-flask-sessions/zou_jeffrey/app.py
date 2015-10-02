971c9c014e08b3fb27999cf6b87d5687fa98c452
JeffreyZou13 <jeffreyzou13@gmail.com>
2015-10-02 12:05:27 -0400

    putting up hw

---
+++ 6/03-flask-sessions/zou_jeffrey/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+from authenticate import authenticate
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        user = request.form['user']
+        password = request.form['pass']
+        if authenticate(user, password):
+            return redirect(url_for('secret'))
+
+@app.route("/logout")
+def logout():
+    session['n'] = 0
+    form['user']=""
+    form['password']=""
+    return render_template("home.html")
+
+@app.route("/secret")
+def secret():
+    if n not in session:
+        session['n']=1
+    return render_template("secret.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.secret_key = "Keyblade"
+    app.run(host="0.0.0.0", port=8000)
