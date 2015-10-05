41af0d0faaf30ca3eda792fbcf0aa8ce52782a4b
atrp-- <dfjkhgshakazulu3091@gmail.com>
2015-10-05 02:07:53 +0000

    Update app.py

--- 5/03-flask-sessions/sosnovsky_alexander/app.py
+++ 5/03-flask-sessions/sosnovsky_alexander/app.py
@@ -6,23 +6,28 @@ app = Flask(__name__)
 
 @app.route("/inc")
 def inc():
+    if 'w' not in session:
+        session['w']=0
+    w=session['w']
+    w=w+1
+    session['w']=w
     if 'n' not in session:
         session['n']=0
     n=session['n']
     n=n+1
     session['n']=n
     print "From a session:",n
-    return render_template("counter.html",n=n)
+    return render_template("counter.html",n=n,w=w)
 
 @app.route("/")
 def index():
-    return '<h1>Welcome to the Sessional Website.</h1><br><button><a href="/inc">Start Your Session</a></button>'
+    return '<h1>Welcome to the Cell Phone Registry.</h1><br><button><a href="/inc">Register Cell Phones</a></button><body background="http://pre15.deviantart.net/d776/th/pre/i/2011/343/f/b/seamless_metal_plate_by_hhh316-d4ink0a.jpg">'
 
 @app.route("/reset")
 def reset():
     session['n']=0
+    session['w']=0
     return redirect("/inc")
-    #return redirect(url_for("inc"))
                     
 
 
