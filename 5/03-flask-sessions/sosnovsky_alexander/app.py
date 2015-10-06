8a566f5a7d1e6313418ba0dcf8da7cb463015c04
atrp-- <dfjkhgshakazulu3091@gmail.com>
2015-10-06 01:29:45 +0000

    Final

--- 5/03-flask-sessions/sosnovsky_alexander/app.py
+++ 5/03-flask-sessions/sosnovsky_alexander/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, session, redirect,url_for, request
-
-
+import urllib
+import ast
 
 app = Flask(__name__)
 
@@ -10,9 +10,34 @@ def inc():
     if 'w' not in session:
         session['w']={}
     w=session['w']
-    #for(x in w.keys()):
-        #s+=x
-        #s+="<br>"
+    wk = w.keys()
+    for x in wk:
+        s+="<br>"
+        s+=x
+        s+="<br>"
+        f = urllib.urlopen("https://ajax.googleapis.com/ajax/services/search/images?q="+x+"&v=1.0")
+        g = f.read()
+        boo = False
+        cnt = 0
+        endcnt = 5
+        while boo == False:
+            if g[cnt] == '.':
+                if g[cnt+1] == 'j':
+                    if g[cnt+2] == 'p':
+                        if g[cnt+3] == 'g':
+                            endcnt = cnt+4
+                            boo = True
+            if boo == False:
+                cnt = cnt+1
+        bcc = False
+        while bcc == False:
+            if g[cnt] == '"':
+                bcc = True
+                cnt = cnt+1
+            else:
+                cnt = cnt-1
+        print g[cnt:endcnt]
+        s+='<img src="'+g[cnt:endcnt]+'" width="50%"><br>'
     session['w']=w
     if 'n' not in session:
         session['n']=0
@@ -20,7 +45,7 @@ def inc():
     n=n+1
     session['n']=n
     print "From a session:",n
-    return render_template("counter.html",n=n,w=w.keys())
+    return render_template("counter.html",n=n,w=s)
 
 @app.route("/register")
 def register():
@@ -33,7 +58,7 @@ def register():
 
 @app.route("/")
 def index():
-    return '<h1>Welcome to the Cell Phone Registry.</h1><br><button><a href="/inc">Register Cell Phones</a></button><body background="http://pre15.deviantart.net/d776/th/pre/i/2011/343/f/b/seamless_metal_plate_by_hhh316-d4ink0a.jpg">'
+    return '<font color="white"><h1>Welcome to the Cell Phone Registry.</font></h1><br><button><a href="/inc">Register Cell Phones</a></button><body background="http://silviahartmann.com/background-tile-art/images/black_woven_seamless_tile.jpg">'
 
 @app.route("/reset")
 def reset():
