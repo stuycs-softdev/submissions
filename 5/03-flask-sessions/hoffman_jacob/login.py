eae7942c50f49a9b10116281eafbddf8f3636fcc
root <root@localhost.localdomain>
2015-10-08 06:30:14 -0400

    fixed my login

--- 5/03-flask-sessions/hoffman_jacob/login.py
+++ 5/03-flask-sessions/hoffman_jacob/login.py
@@ -1,13 +1,9 @@
-from flask import Flask, render_template, session, request
-
-
+from flask import Flask, session, request
 
 login = Flask(__name__)
-@login.route("/failed", methods = ["GET","POST"])
 def failed():
     page = "<h1>You have failed to login :(</h1>"
     return page
-@login.route("/notlogged", methods = ["GET","POST"])
 def notlogged():
     page ="<h1>This is the login page</h1>"
     page = page + '<form method = "POST"><br>Username:<input type = "text" name = "name">'
@@ -15,21 +11,19 @@ def notlogged():
     page = page + '<br>Submit:<input type = "submit" name = "press" value = "login"></form>'
     page = page + '<br> The pass is the answer to this question: If there are 50 apples and you take 10 away, how many apples do you now have? '
     return page
-@login.route("/logged", methods = ["GET","POST"])
-def logged(username):
-    page = '<h1>Nice + ' + username + 'you are not an idiot!! :)</h1>'
+def logged():
+    page = '<h1>Nice '+  session['name'] + ' you are not an idiot!! :)</h1>'
     return page
 def authorized(password):
     return password == '10'
-@login.route("/")
-@login.route("/home", methods = ["GET","POST"])
+@login.route("/home", methods = ["GET", "POST"])
 def home():
-    if 'username' in session.keys():
+    if 'name' in session.keys():
         return logged()
-    elif request.method == "POST" & request.form['press'] == "login":
-        if authorize(request.form('password')):
-            session['username'] = request.form('name')
-            return logged(session['username'])
+    elif request.method == "POST":
+        if (request.form['press'] == 'login') & authorized(request.form['password']):
+            session['name'] = request.form['name']
+            return logged()
         else:
             return failed()
     return notlogged()
