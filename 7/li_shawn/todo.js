8c0d90b4cf4e59d9d65c9dc293583e125ff0cafb
Shawn Li <shawnli050298@gmail.com>
2015-12-02 13:16:50 -0500

    :(

---
+++ 7/li_shawn/todo.js
@@ -0,0 +1,23 @@
+console.log("HELLO, Js loaded");
+
+var addItem = function addItem(s){
+		var l = document.getElementById("thelist");
+		var n = document.createElement("li");
+		n.innerHTML=s;
+		l.appendChild(n);
+};
+
+
+var removeItem = function removeItem(n){
+		var items = document.getElementsByTagName("li");
+		items[n].remove();
+};
+
+var buttonCallback = function(e){
+		addItem("HELLO");
+};
+
+var b2Callback = function(e){
+		e.preventDefault();
+		removeItem(0);
+};
