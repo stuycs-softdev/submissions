9b91bae1185635026323b17ccde53b1786e6d508
MGRiv <mgrivera501@gmail.com>
2015-12-03 02:43:06 -0500

    work required

---
+++ 6/04-javascript-todo/rivera_michael/todo.js
@@ -0,0 +1,25 @@
+console.log("Initiate List");
+
+c = 1;
+
+var button = document.getElementById('button');
+button.addEventListener('click',ButtonCallback);
+
+var ButtonCallback = function(e){
+    var add = document.getElementById("input").value;
+    addItem(add);
+};
+
+var addItem = function addItem(e){
+    var l = document.getElementById("dolist");
+    var a = document.createElement("li");
+    a.id = c++;
+    a.innerHTML = e;
+    a.addEventListener('click',remItem(a.id));
+    l.appendChild(a);
+};
+
+var remItem = function remItem(e){
+    var l = document.getElementById(e);
+    l.remove();
+};
