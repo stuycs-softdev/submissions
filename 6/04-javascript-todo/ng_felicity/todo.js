393216fdf1445e0ce03292ece40225eedbd8b89b
FelicityPictures <felicitypictures@yahoo.com>
2015-12-02 21:26:47 -0500

    working on remove from toDo and add to done

--- 6/04-javascript-todo/ng_felicity/todo.js
+++ 6/04-javascript-todo/ng_felicity/todo.js
@@ -1,39 +1,21 @@
 //the parameter s is the id from the node being removed
 var addItemToDone = function addItemToDone(s){
-    var node = document.createElement("LI");
-    var textnode = document.createTextNode(s);
-    node.appendChild(textnode);
-    document.getElementById("done").appendChild(node); 
-//		var m = document.getElementById(s);
-//		var n = document.getElementById("done");
-//		n.appendChild(m);
+		var n = document.getElementById("done");
+		n.appendChild(s);
 };
 
 var removeItemFromToDo = function removeItemFromToDo(s){
-		var m = document.getElementById(s);
     var n = document.getElementById("todo");
-    n.removeChild(m)
-};
-
-var addItemToDo = function addItemToDone(s){
-		var m = document.getElementById(s);
-		var n = document.getElementById("todo");
-		n.appendChild(m);
-};
-
-var removeItemFromDone = function removeItemFromToDo(s){
-		var m = document.getElementById(s);
-    var n = document.getElementById("done");
-    n.removeChild(m)
+    n.removeChild(s);
 };
 
 //removes item from todo list and adds to done list
 var t1Callback = function(e){
 		console.log(e);
 		addItemToDone(t1);
-    removeItenFromToDo(t1);
+    removeItemFromToDo(t1);
 };
-document.addEventListener('click',t1Callback("t1"));
+document.getElementById("t1").addEventListener('click',t1Callback);
 
 /*
 var b2Callback = function(e){
