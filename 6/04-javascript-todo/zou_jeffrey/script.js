81b89d141fffe4c46378e0dc3bc66c3a6ba73d8c
JeffreyZou13 <jeffreyzou13@gmail.com>
2015-12-03 06:53:50 -0500

    changed some things

--- 6/04-javascript-todo/zou_jeffrey/script.js
+++ 6/04-javascript-todo/zou_jeffrey/script.js
@@ -1,6 +1,3 @@
-/*
-This functions creates a new list item and appends it to the list
-*/
 var addTodo = function addTodo(words) {
   var list = document.getElementById("todo");
   var newItem = document.createElement("li");
@@ -8,15 +5,26 @@ var addTodo = function addTodo(words) {
   list.appendChild(newItem);
 };
 
+var addDone = function addDone(words) {
+  var list = document.getElementById("done");
+  var newItem = document.createElement("li");
+  newItem.innerHTML = words;
+  list.appendChild(newItem);
+};
+
+
 var appendText() = function appendText(id,words) {
-  if (id === "todo") {
+  if (id === "done") {
     addTodo(words);
+  }
+  else {
+    addDone(words);
   }
 };
 
 document.getElementById("li").addEventListener("click", function(event){
   event.preventDefault();
-  var words = 
-  appendText(this.,words);
+  var words =
+  appendText(this.id,words);
   deleteText(this.index);
 });
