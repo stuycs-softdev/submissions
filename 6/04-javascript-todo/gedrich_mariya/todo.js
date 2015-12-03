e2f9df4b16fa0942d38daf4079c609ff7be768be
masha <maryged@gmail.com>
2015-12-03 08:42:50 -0500

    functional

--- 6/04-javascript-todo/gedrich_mariya/todo.js
+++ 6/04-javascript-todo/gedrich_mariya/todo.js
@@ -16,18 +16,18 @@ var removeItem = function removeItem(item) {
 };
 
 //adds item to done list
-var addItemDone = function addItemDone(s) {
+var addItemDone = function addItemDone(item) {
     var list = document.getElementById("done");
-    var item = document.createElement("li");
-    item.innerHTML = s;
+    //var item = document.createElement("li");
+    //item.innerHTML = s;
     list.appendChild(item);
 };
 
 //stuff for add button
 var b1 = document.getElementById("b1");
 //var b1 = document.getElementById("todo");
-console.log(b1);
-b1.addEventListener('click', b1Callback);
+
+
 
 var b1Callback = function(e) {
     var text = document.getElementById("new_task");
@@ -38,13 +38,17 @@ var b1Callback = function(e) {
 
 //stuff for shifty button
 var b2 = document.getElementById("b2");
-b2.addEventListener('click', b2Callback);
+
 
 var b2Callback = function(e) {
     var list = document.getElementById("todo");
     var items = list.children;
-    items[0].remove();
-    addItemDone(items[0].innerHTML);
+    var thing = items[0];
+    thing.remove();
+    addItemDone(thing);
 };
+
+b1.addEventListener('click', b1Callback);
+b2.addEventListener('click', b2Callback);
 
 
