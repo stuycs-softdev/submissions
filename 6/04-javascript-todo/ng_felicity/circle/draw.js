2549d07837ebe34ed66e80160b4e42dc1999d861
FelicityPictures <felicitypictures@yahoo.com>
2015-12-04 12:13:10 -0500

    starting circle game

---
+++ 6/04-javascript-todo/ng_felicity/circle/draw.js
@@ -0,0 +1,4 @@
+var dimension = [document.documentElement.clientWidth, document.documentElement.clientHeight];
+var c = document.getElementById("canvas");
+c.width = dimension[0];
+c.height = dimension[1];
