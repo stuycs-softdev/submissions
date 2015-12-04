bbaf3d2d67b0b1cf5e0ddd213040832a302c2e49
lorenrose1013 <lorenrose1013@aol.com>
2015-12-04 13:03:43 -0500

    todo to done is done

--- 7/04-javascript-todo/loren_maggiore/src.js
+++ 7/04-javascript-todo/loren_maggiore/src.js
@@ -8,9 +8,14 @@ var done = document.getElementById("done");
 var addEvents = function(item){
 	console.log("adding listener");
 	item.addEventListener('click', function(e){
-		console.log("clicked!");
-		item.parentNode.removeChild(item);	
-		done.appendChild(item);
+	console.log("clicked!");
+	var stuff = item.innerHTML;
+	console.log(stuff);
+	var node = document.createElement("LI");              
+	var textnode = document.createTextNode(stuff);         
+	node.appendChild(textnode);                           
+	document.getElementById("done").appendChild(node);
+	item.parentNode.removeChild(item);
 	});
 };
 
