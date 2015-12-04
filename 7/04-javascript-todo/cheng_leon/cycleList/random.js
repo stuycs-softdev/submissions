83d215705ae335370f505689162d82f3914da164
TESLA <57leonardo@gmail.com>
2015-12-04 13:24:29 -0500

    starting with making list elements red

--- 7/04-javascript-todo/cheng_leon/cycleList/random.js
+++ 7/04-javascript-todo/cheng_leon/cycleList/random.js
@@ -14,3 +14,12 @@ var stop = document.getElementById('stop');
 stop.addEventListener('click', function(e){
     clearInterval(myInterval);
 });
+
+var temp = function temp(){
+    var items = document.getElementsByTagName("li");
+    
+    for (var i = 0; i<items.length; i++){
+	items[i].classList.add("red");
+    }
+};
+
