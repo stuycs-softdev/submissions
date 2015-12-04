ea2208c1d98e64172d57b4fa01dfdd5d57621fcc
igerstein <isaac.gerstein1@gmail.com>
2015-12-04 16:32:43 -0500

    Update todo.js

--- 5/04-javascript-todo/gerstein_isaac/todo.js
+++ 5/04-javascript-todo/gerstein_isaac/todo.js
@@ -4,18 +4,33 @@ var add = function add(item, list){
     list.appendChild(tag);
 };
 
+var highlight = -1;
+
 var itemCallback = function itemCallback(e){
     var todo = document.getElementById("todo");
     var done = document.getElementById("done");
     if (done.contains(this)){
 	done.removeChild(this);
     }else{
+	this.style.color = "black";
+	var index = -1;
+	for (var i = 0; i < todo.children.length; i++){
+	    if (todo.children[i] == this){
+		index = i;
+	    }
+	}
+	if (index == highlight){
+	    highlight = -1;
+	}else if (index < highlight){
+	    highlight--;
+	}
 	todo.removeChild(this);
 	done.appendChild(this);
     }
 };
 
-var buttonCallback = function buttonCallback(e){
+var addButton = document.getElementById("add");
+addButton.addEventListener("click", function(e){
     var list = document.getElementById("todo");
     var textBox = document.getElementById("item");
     var input = textBox.value;
@@ -24,7 +39,39 @@ var buttonCallback = function buttonCallback(e){
 	list.children[list.children.length - 1].addEventListener("click", itemCallback);
     }
     textBox.value = "";
+});
+
+var highlightEvent = function highlightEvent(e){
+    var todo = document.querySelector("#todo");
+    if (todo.children.length > 0){
+	if (highlight == -1){
+	    highlight = 0;
+	}else{
+	    todo.children[highlight].style.color = "black";
+	    highlight++;
+	    if (highlight == todo.children.length){
+		highlight = 0;
+	    }
+	}
+	todo.children[highlight].style.color = "red";
+    }
 };
 
-var button = document.getElementById("button");
-button.addEventListener("click", buttonCallback);
+var highlightButton = document.querySelector("#highlight");
+highlightButton.addEventListener("click", highlightEvent);
+
+var running = false;
+
+var startButton = document.querySelector("#start");
+startButton.addEventListener("click", function(e){
+    if (!running){
+	highlightInterval = setInterval(highlightEvent, 1000);
+	running = true;
+    }
+});
+
+var stopButton = document.querySelector("#stop");
+stopButton.addEventListener("click", function(e){
+    clearInterval(highlightInterval);
+    running = false;
+});
