3735d0676ac5d2b2da7493cb91bd7f0d640a774c
daltonwu <dalton.z.wu@gmail.com>
2015-12-04 12:32:11 -0500

    Update

--- 6/04-javascript-todo/wu_dalton/thing.js
+++ 6/04-javascript-todo/wu_dalton/thing.js
@@ -1,2 +1,14 @@
-var addToDo = function(e) {
+var add_todo = function(item) {
+	var list = document.getElementById("list_todo");
+	var new_item = document.createElement("li");
+	new_item.innerHTML = item;
+	list.appendChild(new_item);
 };
+
+var button_add_todo = function(foo) {
+	console.log(foo);
+	addItem("Item x");
+};
+
+var l = document.getElementById("list_todo");
+l.addEventListener("click", button_add_todo);
