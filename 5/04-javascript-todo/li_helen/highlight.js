8db45222f082154ea9b4ccb2411d9f0953469689
Helen Li <helen82198@gmail.com>
2015-12-04 11:47:36 -0500

    highlight

--- 5/04-javascript-todo/li_helen/highlight.js
+++ 5/04-javascript-todo/li_helen/highlight.js
@@ -1,11 +1,31 @@
-ind = 0;
+ind = -1;
 
-var highlight = function highlight(){
+var highlight = function highlight(e){
+    ind++;
     items=document.getElementById("list").children;
-    console.log(items);
-    document.getElementById("list")[ind-1].style.color="black";
-    items[ind].style.color="yellow";  
+    ele = items[ind%items.length];
+    ele.style.color="yellow";
+    ele2 = items[(ind-1)%items.length];
+    ele2.style.color="black";
 };
+
+var highlighter = function highlighter(i){
+    items=document.getElementById("list").children;
+    ele = items[i%items.length];
+    ele.style.color="yellow";
+    ele2 = items[(i-1)%items.length];
+    ele2.style.color="black";
+};
+
+
 
 var b1=document.getElementById("b1");
 b1.addEventListener('click',highlight);
+
+var b2=document.getElementById("b2");
+b2.addEventListener('click',function(e){
+    items=document.getElementById("list").children;
+    for(i=0;i<items.length;i++){
+	window.setTimeout(highlighter(i),1000);
+    }
+});
