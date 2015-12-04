dc2fad06060f81653876f32b003e6736bc69f932
FelicityPictures <felicitypictures@yahoo.com>
2015-12-04 12:29:15 -0500

    testing draw circle

--- 6/04-javascript-todo/ng_felicity/circle/draw.js
+++ 6/04-javascript-todo/ng_felicity/circle/draw.js
@@ -1,4 +1,14 @@
-var dimension = [document.documentElement.clientWidth, document.documentElement.clientHeight];
+/*var score = 0;
 var c = document.getElementById("canvas");
 c.width = dimension[0];
-c.height = dimension[1];
+c.height = dimension[1];*/
+
+//draws a circle
+//Parameters: x coordinate, y coordinate, radius,
+function drawCircle(x,y,r){
+    var c = document.getElementById("canvas");
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, 2 * Math.PI);
+    ctx.stroke();
+}
