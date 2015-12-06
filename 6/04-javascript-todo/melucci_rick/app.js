3cfc3e6721dcb9289e3f109b89c4a7aba5c6277e
Rick Melucci <rimelucci@gmail.com>
2015-12-06 11:30:13 -0500

    added lots and lots more stuff, almost done

--- 6/04-javascript-todo/melucci_rick/app.js
+++ 6/04-javascript-todo/melucci_rick/app.js
@@ -1,3 +1,5 @@
+var slideCount = 0;
+
 var add = function add(item, list){
     var listItem = document.createElement("li");
     listItem.innerHTML = item;
@@ -19,20 +21,95 @@ btn.addEventListener('click', itemAddition);
 
 var purpleHover = document.getElementById('hover1');
 var brownHover = document.getElementById('hover2');
+var yellowHover = document.getElementById('hover3');
 
-purpleHover.addEventListener('onmouseover', function(){
-  if (mouseenter()) {
+var checkHover = function checkHover(){
+  purpleHover.onmouseover = function(){
     purpleHover.style.color = "white";
   }
-});
+  purpleHover.onmouseout = function(){
+    purpleHover.style.color = "";
+  }
 
-brownHover.addEventListener('onmouseenter', function(){
-  if (mouseenter()) {
+  brownHover.onmouseover = function(){
     brownHover.style.color = "brown";
   }
+  brownHover.onmouseout = function(){
+    brownHover.style.color = "";
+  }
+
+  yellowHover.onmouseover = function(){
+    yellowHover.style.color = "yellow";
+  }
+  yellowHover.onmouseout = function(){
+    yellowHover.style.color = "";
+  }
+}
+
+checkHover();
+/*purpleHover.addEventListener('mouseover', function(){
+    purpleHover.style.color = "white";
 });
 
+brownHover.addEventListener('mouseover', function(){
+    brownHover.style.color = "brown";
+});
 
+yellowHover.addEventListener('mouseover', function(){
+    yellowHover.style.color = "yellow";
+});*/
+
+$("#next").click(
+  function () {
+
+    /*if(slideCount == 0){
+      $("#main1").show();
+      slideCount ++;
+    }*/
+
+    if(slideCount == 0){
+      $("#main1").hide(1000);
+      $("#main2").show(300);
+      slideCount ++;
+    }
+
+    else if (slideCount == 2) {
+      $("#main2").hide(1000);
+      slideCount ++;
+    }
+
+    else if (slideCount == 1) {
+      $("#main").fadeOut(1000);
+      $('body').css('background-image', 'url(http://cdn.pitchfork.com/tracks/17609/homepage_large.ee5af306.jpg)');
+      $("#drake").show(1000);
+      $("#body").css('overflow', 'hidden');
+      game();
+    }
+
+});
+
+var game = function game(){
+  document.onkeydown = checkKey;
+}
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '38') {
+        console.log("up")
+    }
+    else if (e.keyCode == '40') {
+        console.log("down");
+    }
+    else if (e.keyCode == '37') {
+       console.log("left");
+    }
+    else if (e.keyCode == '39') {
+       console.log("right");
+    }
+
+}
 
 
 //purpleHover.addEventListener('hover', changeColor("purp"));
