7b0e337fabbbffb464447af1a9bc1693f35a29b0
Rick Melucci <rimelucci@gmail.com>
2015-12-06 13:12:27 -0500

    added a lot to drake snake game but not finished yet, will fix up by tm

--- 6/04-javascript-todo/melucci_rick/app.js
+++ 6/04-javascript-todo/melucci_rick/app.js
@@ -1,3 +1,15 @@
+/*
+  * Rick Melucci
+  *
+  * This is a website with three parts
+  ***The first part is a simple to-do list where one can put in elements
+  ***** I will add a remove elements funtionality as soon as possible
+  ***The second part is to show hovers and addEventListener()
+  ***The third part is a fun little game I tried to make
+  *****Basically a fun little snake game but with meme-starting legend "Drake"
+  * - Lots of things to still fix, will do as soon as possible but made good progress
+*/
+
 var slideCount = 0;
 
 var add = function add(item, list){
@@ -79,17 +91,62 @@ $("#next").click(
     }
 
     else if (slideCount == 1) {
+      var score = 0;
       $("#main").fadeOut(1000);
       $('body').css('background-image', 'url(http://cdn.pitchfork.com/tracks/17609/homepage_large.ee5af306.jpg)');
       $("#drake").show(1000);
       $("#body").css('overflow', 'hidden');
       game();
+      $('#scoreboard').show(1000);
+      number = document.getElementById('score');
+      number.innerHTML = score;
+      window.setTimeout(function(){
+        $('.drakeheader').hide(500);
+        $("#score").show(1000);
+      }, 5000);
+
+
+
+
     }
 
 });
 
+var drake = document.getElementById('drake');
+var moveBox = document.getElementById('move');
+var phone = document.getElementById('phone');
+var divsize = ((Math.random()*100) + 50).toFixed();
+var phoneTop = $("#phone").offset().top,
+    phoneLeft = $("#phone").offset().left,
+    phoneWidth = $("#phone").width(),
+    phoneHeight = $("#phone").height();
+
 var game = function game(){
+  phone.style.width = divsize;
+  phone.style.height = divsize;
+
+  var posx = (Math.random() * $(document).width() - divsize).toFixed();
+  var posy = (Math.random() * $(document).height() - divsize).toFixed();
+
+  phone.style.left = posx + "px";
+  phone.style.top = posy + "px";
+
+  $("#phone").show(300);
+  drake.style.margin = "";
   document.onkeydown = checkKey;
+
+
+}
+
+function isOver(){
+  var drakeLeft = $("#drake").offset().left,
+      drakeTop = $("#drake").offset().top,
+      drakeWidth = $("#drake").width(),
+      drakeHeight = $("#drake").height();
+
+  if((phoneLeft + phoneWidth) > drakeLeft && phoneLeft < (drakeLeft + drakeWidth) && (phoneTop + phoneHeight) > drakeTop && phoneTop < (drakeTop + drakeHeight)){
+    return true;
+  }
 }
 
 function checkKey(e) {
@@ -97,18 +154,41 @@ function checkKey(e) {
     e = e || window.event;
 
     if (e.keyCode == '38') {
-        console.log("up")
+      $("#drake").animate({
+          top: '-=100px'
+      });
     }
     else if (e.keyCode == '40') {
-        console.log("down");
+      $("#drake").animate({
+          top: '+=100px'
+      });
     }
     else if (e.keyCode == '37') {
-       console.log("left");
+      $("#drake").animate({
+          left: '-=100px'
+      });
     }
     else if (e.keyCode == '39') {
-       console.log("right");
+      $("#drake").animate({
+          left: '+=100px'
+      });
     }
 
+    if (drake.style.left < 0 || drake.style.left > $(document).width()){
+       drake.style.left = 0;
+    }
+    else if (drake.style.top < 0 || drake.style.top > $(document).height()) {
+      drake.style.top = 0;
+    }
+
+    if (isOver()){
+      console.log("woo");
+      score += 1;
+      posx = (Math.random() * $(document).width() - divsize).toFixed();
+      posy = (Math.random() * $(document).height() - divsize).toFixed();
+      phone.style.left = posx + "px";
+      phone.style.top = posy + "px";
+    }
 }
 
 
