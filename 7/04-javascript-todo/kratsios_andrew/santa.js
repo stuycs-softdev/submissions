190907b426c90713d7c817c53002aec679b5e230
akratsios <akratsios18@gmail.com>
2015-12-07 01:29:56 -0500

    santa

--- 7/04-javascript-todo/kratsios_andrew/santa.js
+++ 7/04-javascript-todo/kratsios_andrew/santa.js
@@ -1,5 +1,9 @@
 var mouseX;
 var mouseY;
+var deltaYR;
+var deltaXR;
+var deltaYS;
+var deltaXS;
 
  window.addEventListener('mousemove',function(e){
      mouseX=e.pageX;
@@ -16,7 +20,7 @@ function moveS(e) {
    y=y.substring(0,y.length-2);
    y=parseInt(y);
    
-   if (isNaN(x)) x=200;
+   if (isNaN(x)) x=700;
    if (isNaN(y)) y=200;
 
   if (mouseX<x) {
@@ -33,8 +37,8 @@ function moveS(e) {
    moveelt.style.left=x+"px";
    moveelt.style.top=y+"px";
 
-  var deltaYS = mouseY-y;
-  var deltaXS = mouseX-x;
+  deltaYS = mouseY-y;
+  deltaXS = mouseX-x;
   var degs=Math.atan2(deltaYS,deltaXS) *180 / 3.14159;
   santa.style.webkitTransform = "rotate("+degs+"deg)";
 
@@ -43,38 +47,43 @@ function moveS(e) {
 function moveR(e) {
   var rudolf=document.getElementById("rudolf.jpg");
    var moveelt=document.querySelector('.move2');
-   var x = (moveelt.style.left);
-   var y = (moveelt.style.top);
-   x=x.substring(0,x.length-2);
-   x=parseInt(x);
-   y=y.substring(0,y.length-2);
-   y=parseInt(y);
    
-   if (isNaN(x)) x=300;
-   if (isNaN(y)) y=200;
 
    x = mouseX + 2;
    y = mouseY + 2;
    moveelt.style.left=x+"px";
    moveelt.style.top=y+"px";
 
-  var deltaYR = mouseY-y;
-  var deltaXR = mouseX-x;
+  deltaYR = mouseY + 2;
+  deltaXR = mouseX + 2;
   var degs=Math.atan2(deltaYR,deltaXR) *180 / 3.14159;
   rudolf.style.webkitTransform = "rotate("+degs+"deg)";
 
 }
 
+var count = 0;
+function touch(e){
+  if(deltaXS < deltaXR && deltaYS < deltaYR){
+    count ++;
+  }
+}
+
 
 var myevent;
+var myeventR;
+var touchy;
 function startit() {
  myevent = setInterval(moveS,40);
  myeventR = setInterval(moveR,40);
+ touchy = setInterval(touch,10);
+
 }
 
 function stopit() {
 	window.clearTimeout(myevent);
   window.clearTimeout(myeventR);
+  window.clearTimeout(touchy);
+  //document.write(count);
 }
 
  document.getElementById("start").addEventListener('click',startit);
