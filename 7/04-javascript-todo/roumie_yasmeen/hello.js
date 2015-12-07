5034a5eb67829c5e2558bbf62cce21da9ed540c2
Yasmeen Roumie <y4smeen@gmail.com>
2015-12-07 13:16:01 -0500

    idk

--- 7/04-javascript-todo/roumie_yasmeen/hello.js
+++ 7/04-javascript-todo/roumie_yasmeen/hello.js
@@ -1,17 +1,15 @@
-var script = document.createElement('script');
-script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
+
 
 var hellos = ["Hello", "Hola", "Ni Hao", "Salut", "Hallo", "Ciao", "Ahoj", "Hej", "Konichiwa", "Shalom", "Annyeonghaseyo"];
 var colors = ["red", "yellow", "blue"]
 
 $(document).ready(function(){
-  $(this).click(function(e){
+  $("button").on("_mouseDown", function(e){
     var randomHello = hellos[Math.floor(Math.random() * hellos.length)];
     var randomColor = colors[Math.floor(Math.random() * colors.length)];
     var xcor = e.pageX;
     var ycor = e.pageY;
-    $("body").append($('<p style="left: ' + xcor + '; right: ' + ycor '; color: ' + randomColor + '">' + randomHello + '</p>'));
+    $("body").append("<p style='color: red'>Hi</p>");
+    /* $('<p style="left: ' + xcor + '; right: ' + ycor '; color: ' + randomColor + '">' + randomHello + '</p>') */
   });
 });
