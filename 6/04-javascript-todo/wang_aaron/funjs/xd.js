55a333dda9fd7eeac1473ff748b00fb7eb260e71
az0977776 <az0977776@yahoo.com>
2015-12-07 00:45:40 -0500

    catch the ball

--- 6/04-javascript-todo/wang_aaron/funjs/xd.js
+++ 6/04-javascript-todo/wang_aaron/funjs/xd.js
@@ -2,20 +2,37 @@ var mouseX;
 var mouseY;
 var winHeight;
 var winWdith;
+var ball = document.getElementById("ball");
+var ballx;
+var bally;
+
+winHeight = window.innerHeight;
+winWidth = window.innerWidth;
+document.getElementById("windowSize").innerHTML = winHeight + " by " + winWidth;
 
 window.addEventListener("mousemove", function(e) {
     mouseX = e.pageX;
     mouseY = e.pageY;
+    
+    document.getElementById("location").innerHTML = mouseX + ", " + mouseY;
+    winWidth = window.innerWidth;
+});
+
+window.addEventListener("resize", function(e) {
     winHeight = window.innerHeight;
     winWidth = window.innerWidth;
 
-    document.getElementById("location").innerHTML = mouseX + ", " + mouseY;
     document.getElementById("windowSize").innerHTML = winHeight + " by " + winWidth;
 });
 
-var summonX;
-var summonY;
-var summon = function summon(x,y) {
-    summonX;
-};
+ball.style.position = "absolute";
+ball.style.left = "500px";
+ball.style.top = "200px";
+ball.addEventListener("mouseover", function(e) {
+    console.log("enter");
+    var ballx = Math.floor((Math.random() * winWidth) + 1);
+    var bally = Math.floor((Math.random() * winHeight) + 1);
 
+    ball.style.left=ballx + "px"
+    ball.style.top=bally + "px"
+});
