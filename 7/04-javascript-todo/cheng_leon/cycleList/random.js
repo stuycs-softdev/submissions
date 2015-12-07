51bfcb85c0f39b89a6ad37cd09eaa19e9e138eb7
TESLA <57leonardo@gmail.com>
2015-12-07 09:51:00 -0500

    start and next with images

--- 7/04-javascript-todo/cheng_leon/cycleList/random.js
+++ 7/04-javascript-todo/cheng_leon/cycleList/random.js
@@ -2,41 +2,46 @@
 
 index = 0;
 
+
 var colorNext = function color(){
-    var items = document.getElementsByTagName("li");
+    //var items = document.getElementsByTagName("li");
     var pics = document.getElementsByTagName("img");
-    i = index%items.length;
-    items[i].classList.add("red");
+    i = index%pics.length;
+    //items[i].classList.add("red");
     pics[i].height = "200";
-    if(i!=0 && items[i-1].classList.contains("red")){
-	items[i-1].classList.remove("red");
-	pics[i].height = "100";
+    if(i!=0){
+	//items[i-1].classList.remove("red");
+	pics[i-1].height = "100";
     }
-    if(items[0].classList.contains("red") && (items[items.length-1].classList.contains("red"))){
-	items[items.length-1].classList.remove("red");
-	pics[i].height = "200";
+    else {
+	pics[pics.length-1].height = "100";
     }
+    //if(items[0].classList.contains("red") && (items[items.length-1].classList.contains("red"))){
+    //items[items.length-1].classList.remove("red");
+    //pics[i].height = "200";
+    //}
     index++;
+    console.log(i);
 };
 
 var addMouseEvents = function addMouseEvents(item){
     item.addEventListener('click', function(e){
-	for (var j = 0; j<items.length;j++){
-	    if (items[j]==item){
+	for (var j = 0; j<pics.length;j++){
+	    if (pics[j]==pics){
 		break;
 	    }
 	}
-	items[index%items.length-1].classList.remove("red");
-	console.log(index%items.length);
+	pics[index%pics.length-1].height = 100;
+	console.log(pics%items.length);
 	index = j;
 	colorNext();
     });
 };
 
-var items = document.getElementsByTagName("li");
-for (var j = 0; j<items.length;j++){
-    addMouseEvents(items[j]);
-}
+//var items = document.getElementsByTagName("li");
+//for (var j = 0; j<items.length;j++){
+//    addMouseEvents(items[j]);
+//}
 
 
 var myInterval;
