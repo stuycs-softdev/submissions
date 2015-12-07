936207443e47244e7bd1f4b35b15abf9f6c20323
mmcgreal <mmcgreal14@yahoo.com>
2015-12-06 22:57:54 -0500

    santa coming along

--- 7/04-javascript-todo/mcgreal_mary/santa.js
+++ 7/04-javascript-todo/mcgreal_mary/santa.js
@@ -1,43 +1,58 @@
+var mouseX;
+var mouseY;
 
-var thluffy = document.querySelector("#thluffy");
+ window.addEventListener('mousemove',function(e){
+     mouseX=e.pageX;
+     mouseY=e.pageY;
+   }); 
 
-console.log(thluffy);
+function move(e) {
+  var thluffy=document.getElementById("thluffy");
+   var moveelt=document.querySelector('.move');
+   var x = (moveelt.style.left);
+   var y = (moveelt.style.top);
+   x=x.substring(0,x.length-2);
+   x=parseInt(x);
+   y=y.substring(0,y.length-2);
+   y=parseInt(y);
+   
+   if (isNaN(x)) x=200;
+   if (isNaN(y)) y=200;
 
-window.addEventListener('mousemove',function(e){
-		if (e.pageX > 550){
-				thluffy.src = "thluffy-left.png";
-		} else {
-				thluffy.src = "thluffy-right.png";
-		}
-});
+  if (mouseX<x) {
+     x=x-3;
+  } else {
+     x=x+3;
+   }
 
-var count = 1;
-var addItem = function addItem(){
-		var l = document.getElementById("thelist");
-		var n = document.createElement("li");
-		n.innerHTML="Item: "+count;
-		count = count + 1;
-		l.appendChild(n);
-};
+  if (mouseY<y) {
+     y=y-3;
+  } else {
+     y=y+3;
+   }
+   moveelt.style.left=x+"px";
+   moveelt.style.top=y+"px";
 
+  var deltaY = mouseY-y;
+  var deltaX = mouseX-x;
+  var degs=Math.atan2(deltaY,deltaX) *180 / 3.14159;
+  thluffy.style.webkitTransform = "rotate("+degs+"deg)";
 
-var doStuff = function doStuff(){
-		addItem();
-		// setTimeout(doStuff,5000);
-};
+  //('-webkit-transform','rotate('+degs+"deg)");
 
-var myInterval;
-
-var start = document.getElementById("start");
-var stop = document.getElementById("stop");
-start.addEventListener('click',function(){
-		myInterval = setInterval(doStuff,1000);
-});
-stop.addEventListener('click',function(){
-		clearInterval(myInterval);
-});
+}
 
 
+var myevent;
+function startit() {
+ myevent = setInterval(move,100);
+}
+function stopit() {
+	window.clearTimeout(myevent);
+}
+
+ document.getElementById("start").addEventListener('click',startit);
+ document.getElementById("stop").addEventListener('click',stopit);
 
 
 
