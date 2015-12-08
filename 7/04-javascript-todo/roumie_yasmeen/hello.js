cf5da19c8c92f25faabe4b41251c727f031fa0b0
Yasmeen Roumie <y4smeen@gmail.com>
2015-12-07 23:47:54 -0500

    fix

--- 7/04-javascript-todo/roumie_yasmeen/hello.js
+++ 7/04-javascript-todo/roumie_yasmeen/hello.js
@@ -4,12 +4,12 @@ var hellos = ["Hello", "Hola", "Ni Hao", "Salut", "Hallo", "Ciao", "Ahoj", "Hej"
 var colors = ["red", "yellow", "blue"]
 
 $(document).ready(function(){
-  $("button").on("_mouseDown", function(e){
+  $(document).click(function(e){
     var randomHello = hellos[Math.floor(Math.random() * hellos.length)];
     var randomColor = colors[Math.floor(Math.random() * colors.length)];
     var xcor = e.pageX;
     var ycor = e.pageY;
-    $("body").append("<p style='color: red'>Hi</p>");
+    $("body").append("<p style='color: red; position: fixed; left:" + xcor + "'>Hi</p>");
     /* $('<p style="left: ' + xcor + '; right: ' + ycor '; color: ' + randomColor + '">' + randomHello + '</p>') */
   });
 });
