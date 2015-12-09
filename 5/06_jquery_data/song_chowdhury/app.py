6f3454ac52005468a8ba7c3755579621a2e74eb8
David Song <songdavid98@gmail.com>
2015-12-09 18:47:16 -0500

    updated app

--- 5/06_jquery_data/song_chowdhury/app.py
+++ 5/06_jquery_data/song_chowdhury/app.py
@@ -4,6 +4,13 @@ from flask import Flask, render_template, request, redirect, url_for, session
 def index():
     return render_template("index.html")
 
+@app.route("/getprofile")
+def getprofile():
+    print "starting getprofile"
+    print "ending getprofile"
+    return "profile"
+
+
 if __name__ == "__main__":
    app.debug = True
    app.run(host="0.0.0.0", port=8000)
