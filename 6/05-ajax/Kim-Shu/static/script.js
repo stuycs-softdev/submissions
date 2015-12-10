68f5a22835435f13c21d220da77ca085570004a6
shishu1998 <shishu_1998@yahoo.com>
2015-12-10 18:51:55 -0500

    changed keys + added most popular

--- 6/05-ajax/Kim-Shu/static/script.js
+++ 6/05-ajax/Kim-Shu/static/script.js
@@ -2,12 +2,19 @@ var getstuff = function(e){
     var Http = new XMLHttpRequest({mozSystem:true});
     var category = "sports";
     //var category = document.getElementById("form-input").value;
-    var url = "http://api.nytimes.com/svc/topstories/v1/" + category + ".jsonp?callback=callbackTopStories&api-key=8cdfe439659b811ba7bd539c4c047ad5:4:73721357";
+    var url = "http://api.nytimes.com/svc/search/v2/articlesearch" + category + ".jsonp?callback=nyt_biz_news_cb&api-key=024b40b8c1e9ef1333054858a907a687:6:73721357";
     Http.open("GET",url,true);
     Http.send();
     console.log(Http.responseText);
     return Http.responseText;
 };
+
+var rotate = function(e){
+    var Http = new XMLHttpRequest();
+    var category = "sports";
+    //var category = document.getElementById("form-input").value;
+    var url = "http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1[.jsonp]?api-key=610f642ea004ddd5aae700828e5aeec7:1:73721357";
+}
 
 var submit = document.getElementById("form-btn");
 submit.addEventListener('click',getstuff);
