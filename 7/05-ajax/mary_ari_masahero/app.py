660dfea258e7cfbade62a2fd971c0d07faec7498
Masa13 <masahero55@gmail.com>
2015-12-10 13:12:33 -0500

    edited app.py

--- 7/05-ajax/mary_ari_masahero/app.py
+++ 7/05-ajax/mary_ari_masahero/app.py
@@ -1,3 +1,15 @@
-import urllib2
-import json
+import urllib2, json
+from flask import Flask, render_template, request, session, redirect, url_for, escape
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.secret_key = "This is a secret key"
+    app.run('0.0.0.0', port=8000)
 
