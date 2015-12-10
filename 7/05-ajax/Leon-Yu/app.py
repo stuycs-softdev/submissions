af6eb595929a0315045027d1493457b5a71f9bd5
leoncheng57 <57leonardo@gmail.com>
2015-12-10 13:12:29 -0500

    starting app.py

--- 7/05-ajax/Leon-Yu/app.py
+++ 7/05-ajax/Leon-Yu/app.py
@@ -1,17 +1,29 @@
 import urllib2
 import json
+from flask import Flask, render_template, request
+import time, json
 
-
-#url="http://www.nytimes.com"
+app = Flask(__name__)
 
 #url = "http://www.weather.com/weather/5day/l/10001:4:US"
 
-url="http://api.openweathermap.org/data/2.5/weather?q=NYC&appid=ecdb9f3fb43f5f8663867db2633c7638&units=imperial"
-request = urllib2.urlopen(url)
-result = request.read()
-r = json.loads(result)
-print r
-print
-print r.keys()
-print r['main']
-print r['main']['temp']
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+   app.debug = True
+   app.run(host="0.0.0.0", port=8000)
+
+@app.route("/resuts")
+def results():
+    url="http://api.openweathermap.org/data/2.5/weather?q=NYC&appid=ecdb9f3fb43f5f8663867db2633c7638&units=imperial"
+    request = urllib2.urlopen(url)
+    result = request.read()
+    r = json.loads(result)
+    print r
+    print
+    print r.keys()
+    print r['main']
+    return r['main']['temp']
