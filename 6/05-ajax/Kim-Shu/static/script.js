f4c70b62782569b453e41487065d5e6735709ebb
Shi Shu <shishu_1998@yahoo.com>
2015-12-10 13:57:35 -0500

    attempt to fix header

--- 6/05-ajax/Kim-Shu/static/script.js
+++ 6/05-ajax/Kim-Shu/static/script.js
@@ -1,7 +1,5 @@
-
-
-var getstuff function(e){
-    var Http = new XMLHttpRequest();
+var getstuff = function(e){
+    var Http = new XMLHttpRequest({mozSystem:true});
     var category = "sports";
     //var category = document.getElementById("form-input").value;
     var url = "http://api.nytimes.com/svc/topstories/v1/{" + category + "}.{.json}?api-key={8cdfe439659b811ba7bd539c4c047ad5:4:73721357}";
@@ -9,5 +7,8 @@ var getstuff function(e){
     Http.send();
     return Http.responseText;
 };
+
+var submit = document.getElementById("form-btn");
+submit.addEventListener('click',getstuff);
 
 
