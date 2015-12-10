78cc5a067a9ca621df0a263ac74c23667ab970b4
Derrick Lui <derricklui1@gmail.com>
2015-12-10 12:32:10 -0500

    updated

--- 6/05-ajax/felicity-lui/data.js
+++ 6/05-ajax/felicity-lui/data.js
@@ -1,7 +1,27 @@
-function getData(){
-    console.log("getting data");
-    $.get("data.csv",{company}
+$(document).ready(function() {
+    $.ajax({
+        type: "GET",
+        url: "MOCK_DATA.csv",
+        dataType: "text",
+        success: function(data) {processData(data);}
     });
-    console.log("back from getting stuff");
+});
+
+function processData(allText) {
+    var allTextLines = allText.split(/\r\n|\n/);
+    var headers = allTextLines[0].split(',');
+    var lines = [];
+
+    for (var i=1; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+        if (data.length == headers.length) {
+
+            var tarr = [];
+            for (var j=0; j<headers.length; j++) {
+                tarr.push(headers[j]+":"+data[j]);
+            }
+            lines.push(tarr);
+        }
+    }
+    // alert(lines);
 }
-    
