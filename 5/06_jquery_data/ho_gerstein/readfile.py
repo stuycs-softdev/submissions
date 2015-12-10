05ca2a7f916786da14812bc78ff85d005ab32e16
Ho Yin Ho <hoyinho999@gmail.com>
2015-12-10 14:01:33 -0500

    file reading

--- 5/06_jquery_data/ho_gerstein/readfile.py
+++ 5/06_jquery_data/ho_gerstein/readfile.py
@@ -1,8 +1,26 @@
 import csv
-
-def readprofiles:
+import random
+def readprofiles():
     f = open("MOCK_DATA.csv","r")
-    f.read()
     profiles = []
-    for person in f:
-        
+    profile = {}
+    person = f.readlines()
+    while(person != None):
+        person = person.split(",")
+        profile["id"] = person[0]
+        profile["first"] = person[1]
+        profile["last"] = person[2]
+        profile["email"] = person[3]
+        profile["country"] = person[4]
+        profile["ip"] = person[5]
+        profiles.append(profile)
+        person = f.readline()
+    f.close()
+    return profiles
+
+def randomprofile():
+    profiles = readprofiles()
+    profileNum = random.randint(0,99)
+    return profiles[profileNum]
+
+readprofiles()
