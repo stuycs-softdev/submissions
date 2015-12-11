b4263db389bb966ade97af792aeeddd99bdfb630
Jion Fairchild <jijiglobe@yahoo.com>
2015-12-10 22:56:15 -0500

    added images and profile display

--- 5/06_jquery_data/fairchild_wu/static/mycode.js
+++ 5/06_jquery_data/fairchild_wu/static/mycode.js
@@ -1,18 +1,24 @@
-console.log("running!");
+//console.log("running!");
 var editCurrent = function editCurrent(data){
-    console.log(data);
-    console.log("code is about to break");
+   // console.log(data);
+   // console.log("code is about to break");
     var currentProfile = document.getElementById("currentProfile");
     //console.log("profile:");
     //console.log(currentProfile);
     currentProfile.innerHTML = data;
-    console.log(currentProfile);
+//    console.log(currentProfile);
     console.log(currentProfile.innerHTML)
 }
 
+var setImage = function setImage(data){
+    var currentProfile = document.getElementById("currentProfile");
+    currentProfile.innerHTML+=data;
+}
 getNewProfile = function getNewProfile(){
     $.get("/getProfile",editCurrent);	 
+    $.get("/getImage",setImage);
 }
-console.log("getting");
+//console.log("getting");
 getNewProfile();
-    
+
+setInterval(getNewProfile,5000);    
