21420d40534058767f57ce8e3d49f1abbc16d700
ivan <ivlin112@gmail.com>
2015-12-11 00:09:32 -0500

    a

--- 6/05-ajax/rijiu-lin/static/map.js
+++ 6/05-ajax/rijiu-lin/static/map.js
@@ -1,29 +1,39 @@
 var STREET_VIEW_URL = "https://maps.googleapis.com/maps/api/streetview?";
 var KEY = "AIzaSyCtc4ULXKSocmcjjHzp-T78-xH53a0Sz2w";
+
 var map;
+var time;
 
 var mapinit = function mapinit(){
     map = new google.maps.Map(document.getElementById('map-canvas'), {
 	zoom: 2,
 	center: {lat: 0, lng: 0} 
     });
+
+    time = 60;
+
     setInterval(newRound, 60000);
+    setInterval(reduceTime, 1000);
 };
 
 var newRound = function newRound(){
     google.maps.event.clearListeners(map, 'click');
     $.get("/getCoordinates",function (d){
+	d.stopPropogation();
 	document.getElementById("street-view").src = getStreetView(d.lat, d.lng);
 	map.addListener('click', function(e){
 	    checkAnswer(e,answer);
 	});
     });
+
+    time = 60;
 }
 
 var checkAnswer = function checkAnswer(e){
     var dist = $.get("/distance", {lat1: 0, lon1: 0, lat2: e.latLng.lat(), lon2: e.latLng.lng()}, function(distance){
 	console.log(distance);
     });
+    
 };
 
 var getStreetView = function getStreetView(lat, lng){
@@ -33,4 +43,11 @@ var getStreetView = function getStreetView(lat, lng){
     return STREET_VIEW_URL + size + "&" + location + "&" + key;
 }
 
-window.addEventListener('load', mapinit);
+var reduceTime = function reduceTime(){
+    console.log(time);
+    time = time - 1;
+    console.log(time);
+    document.getElementById("time").innerHTML = time;
+}
+
+//window.addEventListener('load', mapinit);
