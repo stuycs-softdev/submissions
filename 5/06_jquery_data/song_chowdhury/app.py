b51b3ed2e24ae1b4aeb018e79cc7410cc30a0101
David Song <songdavid98@gmail.com>
2015-12-10 22:19:10 -0500

    cleaned up

--- 5/06_jquery_data/song_chowdhury/app.py
+++ 5/06_jquery_data/song_chowdhury/app.py
@@ -3,7 +3,7 @@ import json
 
 app = Flask(__name__)
 data = []
-number = 1
+counter = 1
 file = open("MOCK_DATA.csv", 'r')
 lines = file.readlines() #realines() should return list of lines
 for line in lines:
@@ -11,9 +11,6 @@ for line in lines:
     data.append(newlist)
 
 file.close()
-#print data
-#print number
-#it works! print linelist
 
 @app.route("/")
 def index():
@@ -23,20 +20,16 @@ def index():
 def getprofile():
     print "starting getprofile"
     print "ending getprofile"
-    #return "profile"
     return json.dumps(data)              ####you have to have json.dumps
 
 @app.route("/getdata")      ######returns a line from global variable data
 def getdata():
     print "starting getdata"
-    #########################
-    #print data              #no longer empty
-    counter = number
-    line = data[counter]     #for some reason, number is a local variable
-    number += 1
+    line = data[counter]     #for some reason, counter is a local variable
+    counter += 1
     print "ending getdata"
-    if number > 99:
-        return
+    if counter > 99:
+        return "end"
     return json.dump(line)
 
 
