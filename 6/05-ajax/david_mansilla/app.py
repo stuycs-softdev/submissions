65a3adca88636e6845075a23240a1421d4ee733e
David Rothblatt <daroth13@gmail.com>
2015-12-11 12:22:36 -0500

    fixed errorwith top 10 module

--- 6/05-ajax/david_mansilla/app.py
+++ 6/05-ajax/david_mansilla/app.py
@@ -18,7 +18,9 @@ def get_top_10(key_stat):
     with open('stats.csv') as stats:
         reader = csv.DictReader(stats)
         for row in reader:
-            top10.append({'Player': row['Player'], key_stat : row[key_stat]})
+            print row[key_stat]
+            #key_stat_value = int(row[key_stat])
+            top10.append({'Player': row['Player'], key_stat : float(row[key_stat])})
     result = sorted(top10, key=itemgetter(key_stat), reverse=True)
     
     # TOP 10 List Creation
@@ -34,7 +36,7 @@ def niceify_top_10(top10List, key_stat):
     counter = 1
     while counter <= 10:
       r = top10List[counter]
-      res.append( r['Player'] + ": " + r[key_stat] + " " + key_stat )
+      res.append( r['Player'] + ": " + str(r[key_stat]) + " " + key_stat )
       counter+= 1
     return res
 
