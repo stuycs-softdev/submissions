a569ff0a7e24a217dfeb67fadab1b68a44403888
FelicityPictures <felicitypictures@yahoo.com>
2015-12-10 23:00:05 -0500

    added back button

--- 6/05-ajax/felicity-lui/static/data.js
+++ 6/05-ajax/felicity-lui/static/data.js
@@ -1,3 +1,4 @@
+/*
 function getProfit(){
     //getNegative #s from CSV file
     //assuming it's an array
@@ -5,14 +6,13 @@ function getProfit(){
         var pro = document.getElementById("profitLose")
         var x = document.createElement("ul");
         for(int i = 0;i<array.length;i++){
-            var x = document.createElement("li");
+            var y = document.createElement("li");
             x.innerHTML = array[i];
             pro.appendChild(x);
         }
-        
+        addButton();
     }
 }
-
 function getLose(){
     $.get("/getLose",function(array)){
         var pro = document.getElementById("profitLose");
@@ -21,6 +21,7 @@ function getLose(){
             x.innerHTML = array[i];
             pro.appendChild(x);
         }
+        addButton();
     }
 }
 
@@ -31,7 +32,25 @@ function updating(){
     $.get("/getNext",function(data)){
         var table = document.getElementById("profitLose");
         var x = document.createElement("tr");
-        x.innerHTML = "<th>"+companyName+"</th><th>"+companyProfit+"/th>";
+        x.innerHTML = "<td>"+data[0]+"</td><td>"+data[1]+"/td>";
         table.appendChild(x);
     }
 }
+*/
+
+function addButton(){
+    var f = document.getElementById("user");
+    var n = document.createElement("button");
+    var txt = document.createTextNode("Back");
+    n.appendChild(txt);
+    n.id="back";
+    f.appendChild(n);
+    console.log("done");
+    document.getElementById("back").addEventListener('click',remove());
+}
+
+
+
+//var interval;
+//interval = setInterval(updating,5000);
+addButton();
