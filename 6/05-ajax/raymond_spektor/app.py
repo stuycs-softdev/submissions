ac3ea64bc01a4dabdd47117d0006dd79f063d864
Nydroam <rayw.u17@gmail.com>
2015-12-10 21:39:10 -0500

    merged rest of stuff

--- 6/05-ajax/raymond_spektor/app.py
+++ 6/05-ajax/raymond_spektor/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    return render_template("layout.html")
 
 @app.route("/random")
 def random():
