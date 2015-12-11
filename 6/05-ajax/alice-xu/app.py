83d0ad0b2019e6b1dee3961cb5cac47309ac025a
captiosus <royxu652@yahoo.com>
2015-12-11 11:40:19 -0500

    kinda works

--- 6/05-ajax/alice-xu/app.py
+++ 6/05-ajax/alice-xu/app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, request
 import os.path
 import json
 import requests
-import utils
 from pyGTrends import pyGTrends
 
 app = Flask(__name__)
 global connection
 global counter
 connection = None
+counter = 0
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -19,7 +19,7 @@ def index():
             return render_template("index.html", logged=False)
         else:
             pop = popularList()
-            return render_template("index.html", logged=True, popular=pop)
+            return render_template("index.html", logged=true, popular=pop)
     else:
         google_username = request.form['username']
         google_password = request.form['password']
@@ -34,19 +34,17 @@ def index():
 @app.route("/search", methods=['GET'])
 def search():
     if request.method == "GET":
-        # haven't gotten this part to work yet (url returns 400 bad request error)
-        term = request.get_json()
-
-        term = "pizza"
+        term = request.args.get('search')
         data = trend(term)
         results = parseTrend(data)
-    return json.dumps(results)
+        return json.dumps(results)
 
 @app.route("/popular")
 def popular():
     global counter
     pop = popularList()
     data = trend(pop[counter % 20])
+    counter += 1
     r = parseTrend(data)
     return json.dumps(r);
 
@@ -60,27 +58,26 @@ def trend(term):
     return data
 
 def popularList():
-    global counter
     results = requests.get("http://hawttrends.appspot.com/api/terms/")
     r = results.json()
     return r["42"]
 
 def parseTrend(data):
     results = []
-    monthNums = []
-    month = "01"
+    yearNums = []
+    year = "2004"
     for x in range(5, len(data)):
         if (data[x] == ""):
             return results
         else:
             chars = data[x].split(" ")
             dates = chars[2].split("-")
-            if dates[1] != month:
-                month = dates[1]
-                results.append(sum(monthNums) / float(len(monthNums)))
-                monthNums = []
+            if dates[0] != year:
+                year = dates[0]
+                results.append(sum(yearNums) / float(len(yearNums)))
+                yearNums = []
             else:
-                monthNums.append(int((dates[2].split(","))[1]))
+                yearNums.append(int((dates[2].split(","))[1]))
     return results
 
 
--- 6/05-ajax/alice-xu/static/scripts.js
+++ 6/05-ajax/alice-xu/static/scripts.js
@@ -1,58 +1,89 @@
-console.log("loaded js");
-
+var title = document.getElementById("trend-title");
 $("#search").click(function(){
     var input = $("#query");
     var query = input.val();
     input.val("");
-    console.log(query);
-    query = JSON.stringify(query);
     searchQ(query);
 });
 
 var searchData;
 var searchQ = function searchQ(query){
     $.get("/search",{search:query}).done(function(data){
-	alert("data loaded:"+data);
-	console.log("data loaded");
-	searchData = data;
-	getChart(searchData);
+      title.innerHTML = "Google Trends: " + query;
+    	searchData = JSON.parse(data);
+    	getChart(searchData);
     });
 };
 
 
 var yearLabels = [];
 
-var getChart = function getChart() {
+var getChart = function getChart(searchData) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     var ctx = document.getElementById("chart").getContext("2d");
     var year = 2004;
-    for (i=0;i<searchData.length;i++){
-	yearLabels.push(year.toString());
-	year++;
+    for (var i=0;i<searchData.length;i++){
+    	yearLabels.push(year.toString());
+    	year++;
     }
+    var chartData = {
+        labels: yearLabels,
+        datasets: [
+        	{
+        	    label: "Google Trends Data",
+        	    fillColor: "rgba(220,220,220,0.2)",
+        	    strokeColor: "rgba(220,220,220,1)",
+        	    pointColor: "rgba(220,220,220,1)",
+        	    pointStrokeColor: "#fff",
+        	    pointHighlightFill: "#fff",
+        	    pointHighlightStroke: "rgba(220,220,220,1)",
+        	    data: searchData
+        	}
+        ]
+    };
     var lineChart = new Chart(ctx).Line(chartData);
-
 };
 
-var chartData = {
-    labels: yearLabels,
-    // labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-	{
-	    label: "Google Trends Data",
-	    fillColor: "rgba(220,220,220,0.2)",
-	    strokeColor: "rgba(220,220,220,1)",
-	    pointColor: "rgba(220,220,220,1)",
-	    pointStrokeColor: "#fff",
-	    pointHighlightFill: "#fff",
-	    pointHighlightStroke: "rgba(220,220,220,1)",
-	    data: searchData
-	    // data: [28, 48, 40, 19, 86, 27, 90]
-	}
-    ]
+var popInterval;
+var counter;
+var updatePopular = function updatePopular(){
+  counter++;
+  popInterval = setInterval(popular, 5000);
 };
 
-// for popular - not currently used
-var updatePopular = updatePopular(results){
-    searchData = results;
-    getChart();
+var popChart = function popChart(searchData) {
+    var ctx = document.getElementById("pop").getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    var year = 2004;
+    for (var i=0;i<searchData.length;i++){
+    	yearLabels.push(year.toString());
+    	year++;
+    }
+    var chartData = {
+        labels: yearLabels,
+        datasets: [
+        	{
+        	    label: "Google Trends Data",
+        	    fillColor: "rgba(220,220,220,0.2)",
+        	    strokeColor: "rgba(220,220,220,1)",
+        	    pointColor: "rgba(220,220,220,1)",
+        	    pointStrokeColor: "#fff",
+        	    pointHighlightFill: "#fff",
+        	    pointHighlightStroke: "rgba(220,220,220,1)",
+        	    data: searchData
+        	}
+        ]
+    };
+    var lineChart = new Chart(ctx).Line(chartData);
 };
+
+
+function popular() {
+  $.get("/popular", function(data){
+    title.innerHTML = "Google Trends: " + query;
+    searchData = JSON.parse(data);
+    popChart(searchData);
+  });
+}
+
+updatePopular()
