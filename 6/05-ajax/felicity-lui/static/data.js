1f8ef083d634ef14de5e080ddb7de2f7be0f1685
FelicityPictures <felicitypictures@yahoo.com>
2015-12-10 22:09:42 -0500

    js stuff. Fixed get lose/profit

--- 6/05-ajax/felicity-lui/static/data.js
+++ 6/05-ajax/felicity-lui/static/data.js
@@ -1,50 +1,37 @@
-$(document).ready(function() {
-    $.ajax({
-        type: "GET",
-        url: "profit.csv",
-        dataType: "text",
-        success: function(data) {processData(data);}
-    });
-});
-
-function processData(allText) {
-    var allTextLines = allText.split(/\r\n|\n/);
-    var headers = allTextLines[0].split(',');
-    var lines = [];
-
-    for (var i=1; i<allTextLines.length; i++) {
-        var data = allTextLines[i].split(',');
-        if (data.length == headers.length) {
-
-            var tarr = [];
-            for (var j=0; j<headers.length; j++) {
-                tarr.push(headers[j]+":"+data[j]);
-            }
-            lines.push(tarr);
+function getProfit(){
+    //getNegative #s from CSV file
+    //assuming it's an array
+    $.get("/getProfit",function(array)){
+        var pro = document.getElementById("profitLose")
+        var x = document.createElement("ul");
+        for(int i = 0;i<array.length;i++){
+            var x = document.createElement("li");
+            x.innerHTML = array[i];
+            pro.appendChild(x);
         }
-    }
-    // alert(lines);
-}
-
-
-function getProfit(
-//idk if needs parameters
-){
-//getNegative #s from CSV file
-//assuming it's an array
-var pro = document.getElementById("profitLose");
-    for(int i = 0;i<array.length;i++){
-        var x = document.createElement("tr");
-        x.innerHTML = "<th>"+companyName+"</th><th>"+companyProfit+"/th>";
-        pro.appendChild(x);
+        
     }
 }
 
 function getLose(){
-//get negative #s from CSV file
-//same as getProfit but with negative #s. Need parameters?
+    $.get("/getLose",function(array)){
+        var pro = document.getElementById("profitLose");
+        for(int i = 0;i<array.length;i++){
+            var x = document.createElement("li");
+            x.innerHTML = array[i];
+            pro.appendChild(x);
+        }
+    }
 }
-
 
 document.getElementById("profit").addEventListener('click',getProfit);
 document.getElementById("lose").addEventListener('click',getLose);
+
+function updating(){
+    $.get("/getNext",function(data)){
+        var table = document.getElementById("profitLose");
+        var x = document.createElement("tr");
+        x.innerHTML = "<th>"+companyName+"</th><th>"+companyProfit+"/th>";
+        table.appendChild(x);
+    }
+}
