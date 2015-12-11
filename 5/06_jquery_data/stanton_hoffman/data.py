094c45bdcb0cad06db791473ca3b51dbde864cce
Caitlin Stanton <caitlinstanton44@gmail.com>
2015-12-10 23:21:41 -0500

    rand

--- 5/06_jquery_data/stanton_hoffman/data.py
+++ 5/06_jquery_data/stanton_hoffman/data.py
@@ -1,5 +1,6 @@
 import csv
 import random
+
 def readpokedeck():
     fd = open("pokemon.csv","r")
     s = fd.read()[:-1]
@@ -9,33 +10,45 @@ def readpokedeck():
     a = ""
     for pokemon in pokedeck:
         pokemon = pokemon.split(",")
-        pokedeck = {}
-        pokedeck["name"] = pokemon[0]
-        pokedeck["attack"] = pokemon[1]
-        pokedeck["defense"] = pokemon[2]
-        pokedeck["speed"] = pokemon[3]
-        pokedeck["spAtk"] = pokemon[4]
-        pokedeck["spDef"] = pokemon[5]
-		pokedeck["hp"] = pokemon[6]
-		pokedeck["lv"] = pokemon[7]
-        pokedecks.append(pokedeck)
+        newpokemon = {}
+        pokedeck["id"] = pokemon[0]
+        pokedeck["name"] = pokemon[1]
+        pokedeck["attack"] = pokemon[2]
+        pokedeck["defense"] = pokemon[3]
+        pokedeck["speed"] = pokemon[4]
+        pokedeck["spAtk"] = pokemon[5]
+        pokedeck["spDef"] = pokemon[6]
+        pokedeck["hp"] = pokemon[7]
+        pokedeck["lv"] = pokemon[8]
+        pokedeck.append(newpokemon)
     return pokedeck
 
 def randompokemon():
-    pokedecks = readpokedeck()
-    pokedeckNum = random.randint(0,99)
+    pokedeck = readpokedeck()
+    pokedeckNum = random.randint(2,5)
+    print pokedeckNum
     return pokedeck[pokedeckNum]
 
 def searchpokedeck(name):
-    name = name.split()
-    pokedeck = readpokedecks()
-    for pokemon in pokedeck:
-        if pokemon["first"]==name[0] and pokemon["last"]==name[1]:
-            return pokemon
-    return {
-        "id": None,
-        "first": None,
-        "last": None,
-        "email": None,
-        "counttry": None,
-        "ip": None}
+	count = 1
+	tempName = ""
+	while (count < len(name)):
+		if name[count].isupper():
+			tempName = name[:count] + " " + name[:count]
+		count =+ 1
+	name = tempName.split(" ")
+	pokedeck = readpokedecks()
+	for pokemon in pokedeck:
+	    if pokemon["name"]==name[0]:
+		    return pokemon
+	return {
+	    "id": "",
+		"name": "DNE",
+        "attack": "",
+        "defense": "",
+        "speed": "",
+        "spAtk": "",
+        "spDef": "",
+		"hp": "",
+		"lv": ""
+	}
