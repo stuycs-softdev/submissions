ce917bcdd2c5b6995f59ddf0b19d44ddb9bd4dbf
Ariel Levy <ariel.s.levy1@gmail.com>
2015-12-12 14:52:58 -0500

    fixed image height, other details

--- 6/05-ajax/ariel-bao/static/script.js
+++ 6/05-ajax/ariel-bao/static/script.js
@@ -7,7 +7,6 @@ var key = "c1317daa479302572934e2b3f702d523:1:73728355";
 var findList = function findList(){
 		var list = document.getElementById("sellist"); 
 		var listname = list.options[list.selectedIndex].id;
-		var num = document.getElementById("number").value;
 
 		var url = "http://api.nytimes.com/svc/books/v2/lists/" + listname + ".json";
 		
@@ -18,23 +17,25 @@ var findList = function findList(){
 		});
 };
 
-
 var updateList = function updateList(data){
 		console.log(data);
 		var results = data["results"];
 		console.log(results);
 
 		var list = document.getElementById("booklist");
+		$("#booklist").empty();
 
-		for (var i = 0 ; i < results.length ; i++){
+		var num = document.getElementById("number").value;
+
+		for (var i = 0 ; i < results.length && i < num; i++){
 				
 				var details = results[i]["book_details"][0];
 				var title = details["title"];
 				var author = details["author"];
 				var pic = details["book_image"];
 				var descript = details["description"];
-				
-				var el = document.createElement("a");
+
+/*				var el = document.createElement("a");
 				el.classList.add("list-group-item");
 
 				var img = document.createElement("img");
@@ -44,8 +45,15 @@ var updateList = function updateList(data){
 				//console.log(details[0]);
 
 				el.appendChild(img);
-
 				list.appendChild(el);
+*/
+				var item = "<a class='list-group-item'>";
+			  	item += "<img src='"+pic+"' alt='No Image.'>";
+				item += "<h5>"+title+"</h5>";
+				item += "<h6>"+author+"</h6>";
+				item += "</a>";	
+
+				$("#booklist").append(item);
 		}
 };
 
