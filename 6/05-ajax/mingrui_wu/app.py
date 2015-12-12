3f1ffd9afb2dc050dacbda2b836aaa7a6b73bd0e
Dalton Wu <dalton.z.wu@gmail.com>
2015-12-12 18:28:49 -0500

    Update2

--- 6/05-ajax/mingrui_wu/app.py
+++ 6/05-ajax/mingrui_wu/app.py
@@ -1,5 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import json
 
 app = Flask(__name__)
-json
+
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
