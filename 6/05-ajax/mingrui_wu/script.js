8282714677a163c4ceaab353d6eb56887d2826f4
Dalton Wu <dalton.z.wu@gmail.com>
2015-12-13 13:12:19 -0500

    Update!

--- 6/05-ajax/mingrui_wu/script.js
+++ 6/05-ajax/mingrui_wu/script.js
@@ -11,14 +11,37 @@ var load = function load() {
     }});
 };
 
-load();
+var add_row = function add_row(json_num, table_num) {
+    var table = document.getElementById("table");
+    if(table_num < 0) {
+        table_num = table_num + table.rows.length + 1;
+    }
+    var json = JSON.parse(array[json_num]);
+    var row = table.insertRow(table_num);
+    var id = row.insertCell(0);
+    var name = row.insertCell(1);
+    var email = row.insertCell(2);
+    var country = row.insertCell(3);
+    var ip_address = row.insertCell(4);
+    var shirt_color = row.insertCell(5);
+    var credit_card_number = row.insertCell(6);
+    var words = row.insertCell(7);
+    id.innerHTML = json.id;
+    name.innerHTML = json.name;
+    email.innerHTML = json.email;
+    country.innerHTML = json.country;
+    ip_address.innerHTML = json.ip_address;
+    shirt_color.innerHTML = json.shirt_color;
+    credit_card_number.innerHTML = json.credit_card_number;
+    words.innerHTML = json.words;
+};
 
 var change_row = function change_row() {
     setInterval(function() {
-        var n = Math.floor(Math.random() * 1000); // gives int in set [0, 999]
-        var json = JSON.parse(array[n]);
-        console.log(json.name);
+        var json_num = Math.floor(Math.random() * 1000); // gives int in set [0, 999]
+        add_row(json_num, -1);
     }, 1000);
 }
 
+load();
 change_row();
