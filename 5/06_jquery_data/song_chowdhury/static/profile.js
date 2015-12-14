050840430de71be957136e7332c3c2f674a2201e
David Song <songdavid98@gmail.com>
2015-12-14 11:20:40 -0500

    stopped timer after finding profile

--- 5/06_jquery_data/song_chowdhury/static/profile.js
+++ 5/06_jquery_data/song_chowdhury/static/profile.js
@@ -11,14 +11,14 @@ $.get("/getdata", function(d) {
 var getprofile = function getprofile(d) {
     //var data = JSON.parse(d);
     $.get("/getprofile", function(d) {
-		console.log("js getprofile started");
-		var data = JSON.parse(d);
-		console.log("js getprofile ended");
-		$("#first").text(data.first);
-		$("#last").text(data.last);
-		$("#age").text(data.age);
-		$("#email").text(data.email);
-		$("#country").text(data.country);
+	console.log("js getprofile started");
+	var data = JSON.parse(d);
+	console.log("js getprofile ended");
+	$("#first").text(data.first);
+	$("#last").text(data.last);
+	$("#age").text(data.age);
+	$("#email").text(data.email);
+	$("#country").text(data.country);
 	//$("#picture").html("<img src="+ data.picture +" width='300' height='267' alt=''>");
     });
     //console.log(data.value)       //after /getprofile, data loses its value
@@ -26,16 +26,17 @@ var getprofile = function getprofile(d) {
 };
 
 var findprofile = function findprofile(input) {
-	$.get("/findprofile", {data:input}, function(d){
-		console.log("js findprofile started");
-		var data = JSON.parse(d);
-		console.log("js findprofile ended");
-		$("#first").text(data.first);
-		$("#last").text(data.last);
-		$("#age").text(data.age);
-		$("#email").text(data.email);
-		$("#country").text(data.country);
-	});
+    $.get("/findprofile", {data:input}, function(d){
+	console.log("js findprofile started");
+	var data = JSON.parse(d);
+	console.log("js findprofile ended");
+	$("#first").text(data.first);
+	$("#last").text(data.last);
+	$("#age").text(data.age);
+	$("#email").text(data.email);
+	$("#country").text(data.country);
+    });
+    stopit();
 };
 
 var myevent;
@@ -48,9 +49,9 @@ function stopit() {
     window.clearTimeout(myevent);
 }
 function searchit() {
-	console.log("searching person");
-	var input = document.getElementById("input").value;
-	findprofile(input)
+    console.log("searching person");
+    var input = document.getElementById("input").value;
+    findprofile(input)
 }
 document.getElementById("start").addEventListener('click',startit);
 document.getElementById("stop").addEventListener('click',stopit);
