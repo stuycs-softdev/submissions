2011a519f710060ab30e5d63fdfde0ee84feda44
backfrip <backfrip@users.noreply.github.com>
2016-02-03 17:07:20 +0000

    And long after the fact...

--- 7/05-ajax/Shin-Stringham/static/foursquare.js
+++ 7/05-ajax/Shin-Stringham/static/foursquare.js
@@ -8,13 +8,13 @@ $.ajax({
 });
 
 function updatePage(data) {
-  //console.log(Object.keys(data.response.groups[0]));
+  //console.log(Object.keys(data.response.groups[0].items[0].venue.location));
   // ^ Really love this function a whole super lot
   var div = $('#fs');
   div.children('h3').text(data.response.groups[0].type);
   var list = $('<ul>');
   div.append(list);
   $.each(data.response.groups[0].items, function(i, item) {
-    list.append($('<li>').text(item.venue.name));
+    list.append($('<li>').text(item.venue.name + ', ' + item.venue.location.address));
   });
 }
